c75e85f29765feadb393a88d7c1e28ac8bc2fdf1
胡小根 <hxg@haomo-studio.com>
2021-12-09 16:23:07 +0800

    fix: 修复u-icon的bug

--- fix.sh
+++ fix.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cp docs/fixes/uview-ui/mixin.js node_modules/uview-ui/libs/mixin/
 cp docs/fixes/uview-ui/index.js node_modules/uview-ui/
+rsync docs/fixes/uview-ui/components/* node_modules/uview-ui/components/
