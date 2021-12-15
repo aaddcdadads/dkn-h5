0abb54239d35697310a3c8b43e2da36008780762
黄卓然 <huangzhuoran@haomo-studio.com>
2021-12-15 14:54:31 +0800

    feat: 完成HmUviewActionSheet

---
+++ src/components/built-in/HmUviewActionSheet.vue
@@ -0,0 +1,138 @@
+<template>
+  <u-action-sheet
+    v-model="cVisible"
+    :list="list"
+    :tips="tips"
+    :cancelBtn="cancelBtn"
+    :borderRadius="borderRadius"
+    :maskCloseAble="maskCloseAble"
+    :safeAreaInsetBottom="safeAreaInsetBottom"
+    :zIndex="zIndex"
+    :cancelText="cancelText"
+    @click="handleClick"
+  >
+    <slot />
+  </u-action-sheet>
+</template>
+
+<script>
+export default {
+  name: "HmUviewActionSheet",
+  components: {},
+  props: {
+		/**
+		 * 按钮的文字数组
+		 */
+    list: {
+      type: Array,
+      default: function () {
+        return [
+          {
+            text: '点赞',
+            color: 'blue',
+            fontSize: 28,
+            subText: '感谢您的点赞'
+          }, {
+            text: '分享'
+          }, {
+            text: '评论' 
+          }
+        ];
+      },
+    },
+		/**
+		 * 顶部的提示文字
+		 */
+    tips: {
+      type: Object,
+      default: function() {
+				return {
+					text: '在水一方',
+					color: '#909399',
+					fontSize: 24
+				}
+      }
+    },
+    /**
+     * 是否可见
+     */
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+		/**
+		 * 取消按钮文字
+		 */
+    cancelText: {
+      type: String,
+      default: "取消",
+    },
+    /**
+     * 显示取消按钮
+     */
+    cancelBtn: {
+      type: Boolean,
+      default: true,
+    },
+		/**
+     * 弹出部分顶部左右的圆角值
+     */
+    borderRadius: {
+      type: Number,
+      default: 0,
+    },
+		/**
+		 * 点击遮罩是否可以关闭
+		 */
+    maskCloseAble: {
+      type: Boolean,
+      default: true,
+    },
+    /**
+     * 是否开启底部安全区适配
+     */
+    safeAreaInsetBottom: {
+      type: Boolean,
+      default: false,
+    },
+		/**
+		 * z-index值
+		 */
+    zIndex: {
+      type: Number,
+      default: 1075
+    },
+  },
+  data() {
+    return {
+      cVisible: false
+    };
+  },
+  watch: {
+    visible(value) {
+      this.cVisible = !!value;
+    },
+    cVisible(value) {
+      if(value == false) this.handleClose()
+    }
+  },
+  computed: {},
+  mounted() {
+    let self = this;
+    self.cVisible = !!self.visible;
+  },
+  methods: {
+    handleClick(e) {
+      console.log('click: ', e);
+      this.$emit("click", e);
+    },
+    handleClose() {
+      console.log('close: ');
+      this.$emit("close");
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/pages/test/TestHmUviewActionSheet.vue
@@ -0,0 +1,61 @@
+<template>
+	<view>
+		<hm-uview-button
+		text="默认"
+		@click="visible1 = true"
+		></hm-uview-button>
+		<br/>
+		<hm-uview-button
+		text="带属性"
+		@click="visible2 = true"
+		></hm-uview-button>
+		
+		<hm-uview-action-sheet
+		:visible="visible1"
+		@close="visible1 = false"
+		></hm-uview-action-sheet>
+		<hm-uview-action-sheet
+		:tips="tips"
+		:visible="visible2"
+		:list="list"
+		cancelText="cancel"
+		:cancelBtn="true"
+		:borderRadius="10"
+		:maskCloseAble="false"
+		:safeAreaInsetBottom="true"
+		:zIndex="2000"
+		@close="visible2 = false"
+		></hm-uview-action-sheet>
+	</view>
+</template>
+
+<script>
+	import HmUviewActionSheet from "@/components/built-in/HmUviewActionSheet.vue";
+	import HmUviewButton from "@/components/built-in/HmUviewButton.vue";
+
+	export default {
+		components: { HmUviewActionSheet, HmUviewButton },
+		data() {
+			return {
+				visible1: false,
+				visible2: false,
+				list: [
+          {
+            text: 'test1',
+          }, {
+            text: 'test2'
+          }, {
+            text: 'test3' 
+          }
+        ],
+				tips: {
+					text: '提示',
+				}
+			};
+		},
+	}
+</script>
+
+<style lang="less">
+
+</style>
