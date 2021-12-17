e963646f92595dd75b500339db39f58f7cd0d8c5
胡小根 <hxg@haomo-studio.com>
2021-12-17 11:09:46 +0800

    chore: add fix server node_modules

--- fix.sh
+++ fix.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-cp docs/fixes/uview-ui/mixin.js node_modules/uview-ui/libs/mixin/
-cp docs/fixes/uview-ui/index.js node_modules/uview-ui/
-rsync docs/fixes/uview-ui/components/* node_modules/uview-ui/components/
+rsync -avz docker/client.mjs root@sz-server8:/data/docker/design-cloud.dev/shared-node-modules/uniapp-uview-vue3/node-modules/vite/dist/client/
+rsync -avz docker/server.js root@sz-server8:/data/docker/design-cloud.dev/shared-node-modules/uniapp-uview-vue3/node-modules/@dcloudio/vite-plugin-uni/dist/cli/
