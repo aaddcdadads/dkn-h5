a133828f93d14dd0703299c146c050345369dea6
黄卓然 <huangzhuoran@haomo-studio.com>
2021-12-18 11:11:43 +0800

    fix: 修改deploy.sh

--- built-in-deploy.sh
+++ built-in-deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-rsync -avz src/components/built-in/uniapp-uview-vue3/* haomo@192.168.1.7:/data/docker/design-cloud/files/built-in
-rsync -avz src/components/built-in/uniapp-uview-vue3/* haomo@192.168.1.7:/data/docker/design-cloud.stage/files/built-in
-rsync -avz src/components/built-in/uniapp-uview-vue3/* haomo@192.168.1.7:/data/docker/design-cloud.dev/files/built-in
-rsync -avz src/components/built-in/uniapp-uview-vue3/* haomo@192.168.1.8:/data/docker/design-cloud.dev/files/built-in
+rsync -avz src/components/built-in/uniapp-uview-vue3 haomo@192.168.1.7:/data/docker/design-cloud/files/built-in
+rsync -avz src/components/built-in/uniapp-uview-vue3 haomo@192.168.1.7:/data/docker/design-cloud.stage/files/built-in
+rsync -avz src/components/built-in/uniapp-uview-vue3 haomo@192.168.1.7:/data/docker/design-cloud.dev/files/built-in
+rsync -avz src/components/built-in/uniapp-uview-vue3 haomo@192.168.1.8:/data/docker/design-cloud.dev/files/built-in
