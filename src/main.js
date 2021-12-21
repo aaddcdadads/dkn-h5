99cabfbee7ae88187ce85631fdde6da9c815a7cd
黄卓然 <huangzhuoran@haomo-studio.com>
2021-12-21 17:23:11 +0800

    feat: 添加内置组件动态加载js

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import {
 import App from "./App.vue";
 
 import uView from './uni_modules/vk-uview-ui';
+import loadBuiltInComponent from './utils/loadBuiltInComponent';
 
 export function createApp() {
 	const app = createSSRApp(App);
 
 	app.use(uView);
+	//加载内置组件
+	app.use(loadBuiltInComponent);
 
 	return {
 		app,
