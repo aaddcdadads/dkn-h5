0e1e5ed309a6acd8e246b316195c15bc7be3e893
肖禹鑫 <xiaoyuxin@haomo-studio.com>
2022-01-06 18:55:12 +0800

    fix: 添加HmUviewUpload组件

---
+++ src/components/built-in/uniapp-uview-vue3/HmUviewUpload.vue
@@ -0,0 +1,251 @@
+<template>
+  <view>
+    <u-upload
+      :action="cAction"
+      :file-list="cFileList"
+      :name="name"
+      :max-count="maxCount"
+      :width="width"
+      :height="height"
+      :show-progress="showProgress"
+      :disabled="disabled"
+      :preview-full-image="previewFullImage"
+      :deletable="deletable"
+      :max-size="maxSize"
+      :upload-text="uploadText"
+      :auto-upload="autoUpload"
+      :show-tips="showTips"
+      :show-upload-list="showUploadList"
+      :del-icon="delIcon"
+      :del-bg-color="delBgColor"
+      :delColor="delColor"
+      :index="index"
+      @on-remove="onRemove"
+      @on-success="onSuccess"
+      @on-change="onChange"
+      @on-error="onError"
+      @on-uploaded="onUploaded"
+      @on-choose-complete="onChooseComplete"
+    ></u-upload
+  ></view>
+</template>
+
+<script>
+export default {
+  components: {},
+  name: "HmUviewUpload",
+
+  props: {
+    /**
+     * 服务器上传地址
+     */
+    action: {
+      type: String,
+      default: "",
+    },
+    /**
+     * 上传文件名
+     */
+    name: {
+      type: String,
+      default: "file",
+    },
+    /**
+     * 上传图片最大数量
+     */
+    maxCount: {
+      type: String,
+      default: "99",
+    },
+    /**
+     * 图片宽度
+     */
+    width: {
+      type: String,
+      default: "200",
+    },
+    /**
+     * 图片高度
+     */
+    height: {
+      type: String,
+      default: "200",
+    },
+    /**
+     * 是否显示进度条
+     */
+    showProgress: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * 是否隐藏
+     */
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+
+    /**
+     * 是否预览图片
+     */
+    previewFullImage: {
+      type: Boolean,
+      default: true,
+    },
+    /**
+     * 是否显示删除图片按钮
+     */
+    deletable: {
+      type: Boolean,
+      default: true,
+    },
+    /**
+     * 图片最大大小(byte)
+     */
+    maxSize: {
+      type: String,
+      default: "Number.MAX_VALUE",
+    },
+    /**
+     * 默认上传图片
+     */
+    fileList: {
+      type: Array,
+      default: function () {
+        return [];
+      },
+    },
+    /**
+     * 选择图片提示文字
+     */
+    uploadText: {
+      type: String,
+      default: "选择图片",
+    },
+    /**
+     * 自动上传
+     */
+    autoUpload: {
+      type: Boolean,
+      default: true,
+    },
+    /**
+     * 提示
+     */
+    showTips: {
+      type: Boolean,
+      default: true,
+    },
+    /**
+     * 显示图片预览
+     */
+    showUploadList: {
+      type: Boolean,
+      default: true,
+    },
+    /**
+     * 右上角图标样式
+     */
+    delIcon: {
+      type: String,
+      default: "close",
+    },
+    /**
+     * 右上角图标背景颜色
+     * @type Color
+     */
+    delBgColor: {
+      type: String,
+      default: "#fa3534",
+    },
+    /**
+     * 右上角图标颜色
+     * @type Color
+     */
+    delColor: {
+      type: String,
+      default: "#ffffff",
+    },
+    /**
+     * 回调事件的最后一个参数
+     */
+    index: {
+      type: String,
+      default: "1",
+    },
+  },
+  data() {
+    return {
+      cAction: null,
+      cFileList: [
+        {
+          url: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2Ftp05%2F19100120461512E-0-lp.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1644055421&t=e70c7723874c5e040900e75daded2cb4",
+        },
+      ],
+    };
+  },
+  watch: {
+    fileList(value) {
+      this.cFileList = value;
+    },
+    action(value) {
+      this.cAction = this.convertUrl(value);
+    },
+  },
+  mounted() {
+    this.cAction = this.convertUrl(this.action);
+    this.cFileList = this.cFileList;
+  },
+  methods: {
+    convertUrl(url) {
+      if (!url) {
+        return null;
+      }
+      if (window.location.origin.indexOf("block-design.cn") < 0) {
+        return url;
+      }
+      let urlObj = new URL(url);
+      let schema = urlObj.origin.split(":")[0];
+      let port = urlObj.port || (schema === "http" ? 80 : 443);
+      VueCookieNext.setCookie("x-project-api-schema", schema);
+      VueCookieNext.setCookie("x-project-api-hostname", urlObj.hostname);
+      VueCookieNext.setCookie("x-project-api-port", port);
+      return `/project-api${urlObj.pathname}`;
+    },
+    //移除图片时触发
+    onRemove(index, lists, name) {
+      //console.log("onRemove", index, lists, name);
+      this.$emit("onRemove", index, lists, name);
+    },
+    //图片上传成功时触发
+    onSuccess(data, index, lists, name) {
+      //console.log("onSuccess", data, index, lists, name);
+      this.$emit("onSuccess", data, index, lists, name);
+    },
+    //图片上传后，无论成功或者失败都会触发
+    onChange(res, index, lists, name) {
+      // console.log("onChange", res, index, lists, name);
+      this.$emit("onChange", res, index, lists, name);
+    },
+    //图片上传失败时触发
+    onError(res, index, lists, name) {
+      // console.log("onError", res, index, lists, name);
+      this.$emit("onError", res, index, lists, name);
+    },
+    //所有图片上传完毕触发
+    onUploaded(lists, name) {
+      //console.log("onUploaded", lists, name);
+      this.$emit("onUploaded", lists, name);
+    },
+    //每次选择图片后触发
+    onChooseComplete(lists, name) {
+      //console.log("onChooseComplete", lists, name);
+      this.$emit("onChooseComplete", lists, name);
+    },
+  },
+};
+</script>
+
+<style lang="less">
+</style>
---
+++ src/pages/test/TestHmUviewUpload.vue
@@ -0,0 +1,52 @@
+<template>
+  <view>
+    <hm-uview-upload></hm-uview-upload>
+    <hm-uview-upload
+      action="http://www.example.com/upload"
+      :fileList="cFileList"
+      name="names"
+      maxCount="55"
+      width="300"
+      height="300"
+      :showProgress="false"
+      :disabled="false"
+      :previewFullImage="false"
+      :deletable="true"
+      maxSize="5 * 1024 * 1024 "
+      uploadText="请上传"
+      :autoUpload="false"
+      :showTips="false"
+      :showUploadList="true"
+      delIcon="close"
+      delBgColor="#ffffff"
+      delColor="#fa3534"
+      index="2"
+      @onRemove="remove"
+    ></hm-uview-upload>
+  </view>
+</template>
+
+<script>
+import HmUviewUpload from "@/components/built-in/uniapp-uview-vue3/HmUviewUpload.vue";
+export default {
+  name: "TestHmUviewUpload",
+  components: { HmUviewUpload },
+  data() {
+    return {
+      cFileList: [
+        {
+          url: "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2Ftp05%2F19100122420C335-0-lp.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1644055421&t=8a17927351c6025dc8c1b191fdeb4aa0",
+        },
+      ],
+    };
+  },
+  methods: {
+    remove(index, lists, name) {
+      console.log("onRemove", index, lists, name);
+    },
+  },
+};
+</script>
+
+<style>
+</style>
