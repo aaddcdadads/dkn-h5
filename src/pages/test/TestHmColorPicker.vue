37b5fd8bbe430b9b3cd4b5b951e6cc2905694353
黄卓然 <huangzhuoran@haomo-studio.com>
2022-01-15 17:08:21 +0800

    fix: 添加颜色选择器组件

---
+++ src/pages/test/TestHmColorPicker.vue
@@ -0,0 +1,31 @@
+<template>
+	<view>
+		<hm-color-picker
+		
+		></hm-color-picker>
+		<!-- <hm-color-picker
+			:value="value"
+			@colorChange="colorChange"
+		></hm-color-picker> -->
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			value: '#693939FF'
+		};
+	},
+	methods:{
+		colorChange(val){
+			console.log('val',val)
+			this.value = val
+		}
+	}
+}
+</script>
+
+<style lang="less">
+
+</style>
