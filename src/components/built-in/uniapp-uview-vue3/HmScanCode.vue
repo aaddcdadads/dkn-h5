00620030b14f3118cf250993c5a5ee02a2868c05
肖禹鑫 <xiaoyuxin@haomo-studio.com>
2022-01-20 22:51:37 +0800

    fix: 添加HmScanCode组件

---
+++ src/components/built-in/uniapp-uview-vue3/HmScanCode.vue
@@ -0,0 +1,53 @@
+<template >
+  <view>
+    <view class="code"></view>
+  </view>
+</template>
+
+<script>
+export default {
+  name: "HmScanCode",
+  props: {
+    /**
+     * 是否可见
+     */
+    show: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {};
+  },
+
+  onload() {},
+  mounted() {
+    let self = this;
+    setTimeout(function () {
+      if (this.show) {
+        self.scanCode();
+      }
+    }, 200);
+  },
+  methods: {
+    scanCode() {
+      uni.scanCode({
+        success: function (res) {
+          ///console.log(JSON.stringify(res));
+          this.$emit("onDecode", res);
+        },
+        fail: function () {
+          this.$emit("onReturn");
+        },
+      });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.code {
+  width: 100%;
+  height: 100vh;
+}
+</style>
