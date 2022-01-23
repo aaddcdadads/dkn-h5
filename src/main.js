dde616cb969de3d075145319bab7ea27e68b8f42
胡小根 <hxg@haomo-studio.com>
2022-01-23 12:30:24 +0800

    chore: 修改条件编译

--- src/main.js
+++ src/main.js
@@ -1,41 +1,41 @@
 import {
-    createSSRApp
+  createSSRApp
 } from "vue";
 import App from "./App.vue";
 
 import uView from './uni_modules/vk-uview-ui';
 
-#ifdef H5
+// #ifdef H5
 import loadComponent from './utils/loadComponent';
-#endif
+// #endif
 
 import "./mock/index.js";
 
 import {
-    getAction,
-    downloadFile,
-    postAction,
-    deleteAction,
-    putAction,
+  getAction,
+  downloadFile,
+  postAction,
+  deleteAction,
+  putAction,
 } from "./request/http";
 
 export function createApp() {
-    const app = createSSRApp(App);
+  const app = createSSRApp(App);
 
-    app.config.globalProperties.$getAction = getAction;
-    app.config.globalProperties.$downloadFile = downloadFile;
-    app.config.globalProperties.$postAction = postAction;
-    app.config.globalProperties.$deleteAction = deleteAction;
-    app.config.globalProperties.$putAction = putAction;
+  app.config.globalProperties.$getAction = getAction;
+  app.config.globalProperties.$downloadFile = downloadFile;
+  app.config.globalProperties.$postAction = postAction;
+  app.config.globalProperties.$deleteAction = deleteAction;
+  app.config.globalProperties.$putAction = putAction;
 
-	app.use(uView);
+  app.use(uView);
 
-	//加载组件
-	#ifdef H5
-	app.use(loadComponent);
-	#endif
+  //加载组件
+  // #ifdef H5
+  app.use(loadComponent);
+  // #endif
 
-    return {
-        app,
-    };
+  return {
+    app,
+  };
 }
