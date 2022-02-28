cfdf42e51513d829286722768eb091fae6a0ad41
肖君安 <xiaojunan@haomo-studio.com>
2022-02-28 17:48:11 +0800

    fix:增加图表组件

---
+++ src/components/built-in/qiun-data-charts/HmRingCharts.vue
@@ -0,0 +1,96 @@
+<template>
+  <view class="charts-box">
+    <qiun-data-charts
+      type="ring"
+      :chartData="chartData"
+      :rotate="rotate"
+      background="none"
+    />
+  </view>
+</template>
+<script>
+export default {
+  name: "HmColumnCharts",
+  props: {
+    /**
+     * 宽度
+     */
+    width: {
+      type: String,
+      default: "100%",
+    },
+    /**
+     * 高度
+     */
+    height: {
+      type: String,
+      default: "300rpx",
+    },
+    /**
+     * 横屏模式
+     */
+    rotate: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * 图表数据
+     */
+    chartData: {
+      type: Object,
+      default: function () {
+        return {
+          series: [
+            {
+              data: [
+                {
+                  name: "一班",
+                  value: 50,
+                },
+                {
+                  name: "二班",
+                  value: 30,
+                },
+                {
+                  name: "三班",
+                  value: 20,
+                },
+                {
+                  name: "四班",
+                  value: 18,
+                },
+                {
+                  name: "五班",
+                  value: 8,
+                },
+              ],
+            },
+          ],
+        };
+      },
+    },
+  },
+  data() {
+    return {
+      cWidth: "100%",
+      cHeight: "300rpx",
+    };
+  },
+  mounted() {
+    this.cWidth = this.$getCssUnit(this.width);
+    this.cHeight = this.$getCssUnit(this.height);
+  },
+  methods: {
+    onClick(e) {
+      this.$emit("click", e);
+    },
+  },
+};
+</script>
+
+<style lang="less">
+.charts-box {
+  width: v-bind(cWidth);
+  height: v-bind(cHeight);
+}
+</style>
