8cd7659285395e49c7f6ad5691f2e76f2f9edac5
胡小根 <hxg@haomo-studio.com>
2022-05-24 17:12:44 +0800

    fix: 修复entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,6 +8,7 @@ git add src/pages && git add src/pages.json && git commit -m "chore: restart and
 git stash -u && git pull
 rm -rf /root/code-live-preview/node_modules/.vite/*.js.map
 cp /root/code-live-preview/docker/action.js /root/code-live-preview/node_modules/@dcloudio/vite-plugin-uni/dist/cli/action.js
+cp /root/code-live-preview/docker/client.mjs /root/code-live-preview/node_modules/vite/dist/client/
 # npm run serve
 # node node_modules/vite/bin/vite.js
 npm run dev:h5 -- --host `hostname`
