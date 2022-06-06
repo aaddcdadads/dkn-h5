468fddf1e86f4aa1ac38f54168528d66e4de9816
肖禹鑫 <xiaoyuxin@haomo-studio.com>
2022-06-06 20:33:10 +0800

    fix: 添加全局flowUtils方法

--- src/main.js
+++ src/main.js
@@ -35,10 +35,10 @@ export function createApp() {
     app.config.globalProperties.$deleteAction = deleteAction;
     app.config.globalProperties.$putAction = putAction;
 
-    app.config.globalProperties.$map = map;
+    /*app.config.globalProperties.$map = map;
     app.config.globalProperties.$filter = filter;
     app.config.globalProperties.$reduce = reduce;
-    app.config.globalProperties.$array2tree = array2tree;
+    app.config.globalProperties.$array2tree = array2tree;*/
 
     app.config.globalProperties.$getCssUnit = function (value) {
         if (isNaN(Number(value))) {
