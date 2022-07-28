648de25f881fbd7f44e0cc9ad8e1e946a00ff40f
lilongquan <lilongquan@haomo-studio.com>
2022-07-28 17:44:12 +0800

    fix:提交uniapp-Audio组件

--- src/components/built-in/uniapp/HmAudio.vue
+++ src/components/built-in/uniapp/HmAudio.vue
@@ -1,39 +1,76 @@
 <template>
   <view>
-    <view class="page-body">
-      <view class="page-section page-section-gap" style="text-align: center">
-        <audio
-          style="text-align: left"
-          :src="current.src"
-          :poster="current.poster"
-          :name="current.name"
-          :author="current.author"
-          :action="audioAction"
-          controls
-        ></audio>
-      </view>
-    </view>
+    <audio
+      :id="id"
+      :src="src"
+      :loop="loop"
+      :controls="controls"
+      :poster="poster"
+      :name="name"
+      :author="author"
+    ></audio>
   </view>
 </template>
 <script>
 export default {
   data() {
-    return {
-      current: {
-        poster:
-          "https://bjetxgzv.cdn.bspapp.com/VKCEYUGU-uni-app-doc/7fbf26a0-4f4a-11eb-b680-7980c8a877b8.png",
-        name: "致爱丽丝",
-        author: "暂无",
-        src: "https://bjetxgzv.cdn.bspapp.com/VKCEYUGU-hello-uniapp/2cc220e0-c27a-11ea-9dfb-6da8e309e0d8.mp3",
-      },
-      audioAction: {
-        method: "pause",
-      },
-    };
+    return {};
   },
   components: {},
   name: "HmAudio",
-  props: {},
+  props: {
+    /**
+     * 组件唯一标识
+     */
+    id: {
+      type: String,
+      defalut: "",
+    },
+    /**
+     * 音频资源地址
+     */
+    src: {
+      type: String,
+      defalut:
+        "https://bjetxgzv.cdn.bspapp.com/VKCEYUGU-hello-uniapp/2cc220e0-c27a-11ea-9dfb-6da8e309e0d8.mp3",
+    },
+    /**
+     * 是否循环播放
+     */
+    loop: {
+      type: Boolean,
+      defalut: false,
+    },
+    /**
+     * 显示默认控件
+     */
+    controls: {
+      type: Boolean,
+      defalut: false,
+    },
+    /**
+     * 图片资源地址
+     */
+    poster: {
+      type: String,
+      default:
+        "https://bjetxgzv.cdn.bspapp.com/VKCEYUGU-uni-app-doc/7fbf26a0-4f4a-11eb-b680-7980c8a877b8.png",
+    },
+    /**
+     * 音频名字
+     */
+    name: {
+      type: String,
+      default: "致爱丽丝",
+    },
+    /**
+     * 作者名字
+     */
+    author: {
+      type: String,
+      default: "暂无",
+    },
+  },
   methods: {},
 };
 </script>
