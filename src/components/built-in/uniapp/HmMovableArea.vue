a434f30bae31e5e57186c58d072c1b9bf7d23369
刘晶蕊 <huangjihui@l8liujingrui-studio.com>
2022-08-01 14:42:43 +0800

    fix:添加组件cover-image,cover-view

---
+++ src/components/built-in/uniapp/HmMovableArea.vue
@@ -0,0 +1,83 @@
+<template>
+  <view>
+    <label>
+      <movable-area class="alVideo" :scale-area="scaleArea">
+        <movable-view
+          :x="x"
+          :y="y"
+          direction="all"
+          @change="onChange"
+          class="area"
+        >
+        </movable-view>
+      </movable-area>
+    </label>
+  </view>
+</template>
+<script>
+export default {
+  name: 'HmMovableArea',
+  props: {
+    /**
+     * 页面宽度
+     */
+    width: {
+      type: Number,
+      default: 500,
+    },
+    /**
+     * 页面高度
+     */
+    height: {
+      type: Number,
+      default: 500,
+    },
+    /**
+     * 最小宽度
+     */
+    scaleArea: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      cWidth: '',
+      cHeight: '',
+      x: 0,
+      y: 0,
+      old: {
+        x: 0,
+        y: 0,
+      },
+    }
+  },
+  methods: {
+    getCssUnit(value) {
+      if (isNaN(Number(value))) {
+        return value
+      }
+      return `${value}px`
+    },
+    onChange: function (e) {
+      this.old.x = e.detail.x
+      this.old.y = e.detail.y
+    },
+  },
+  mounted() {
+    this.cWidth = this.width
+    this.cHeight = this.height
+  },
+  watch: {
+    width(value) {
+      this.cWidth = getCssUnit(value)
+    },
+    height(value) {
+      this.cHeight = getCssUnit(value)
+    },
+  },
+}
+</script>
+<style lang="less">
+
+</style>
