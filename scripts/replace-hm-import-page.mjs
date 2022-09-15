362d375c00514ba0aab3386cb81b3d364533c6bc
胡小根 <hxg@haomo-studio.com>
2022-09-15 12:02:01 +0800

    feat: 重构替换page.vue中的HmImportPage标签的功能

--- scripts/replace-hm-import-page.mjs
+++ scripts/replace-hm-import-page.mjs
@@ -1,14 +1,22 @@
 #!/usr/bin/env zx
-let hmImportPages = ['1566734066567352321']
-let pageIdMap = {
-  '1566734066567352321': 'pagexxx'
-}
-let hmImportPageRegex = /<hm-import-page[^<]*1566734066567352321[^\/]*<\/hm-import-page>/gs;
-hmImportPages.forEach(async (page) => {
-  let pageContent = await $`cat src/pages/${page}/page.vue`;
-  pageContent = pageContent.stdout;
-  pageContent.replace(hmImportPageRegex, `<${pageIdMap[page]} />`);
-  await $`echo ${pageContent} > src/pages/${page}/page.vue`;
+import _ from 'lodash'
+
+let stat = fs.readFileSync('../docs/stat-hm-import-page.json')
+
+Object.keys(stat).forEach(async (pageId) => {
+  let pageContent = fs.readFileSync(`../src/pages/${pageId}/page.vue`, 'utf8');
+  // /<hm-import-page[^<]*1566734066567352321[^\/]*<\/hm-import-page>/gs
+  let importPageIds = Object.keys(stat[pageId]);
+  if (!importPageIds.length) {
+    return;
+  }
+
+  importPageIds.forEach(importPageId => {
+    let hmImportPageRegex = new RegExp(`<hm-import-page[^<]*${importPageId}[^\/]*<\/hm-import-page>`, 'gs');
+    pageContent.replace(hmImportPageRegex, `<${stat[pageId][importPageId]} />`);
+  })
+  
+  fs.writeFileSync(`../src/pages/${pageId}/page.vue`, pageContent, {encoding: 'utf8', flag: 'w+'});
 })
 
 
