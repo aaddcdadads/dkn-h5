612d44affd9f83a0484d0d3cc2bf15da0a493668
胡小根 <hxg@haomo-studio.com>
2022-09-15 11:07:45 +0800

    feat: 修改统计HmImportPage使用的统计脚本

--- scripts/stat-hm-import-page.mjs
+++ scripts/stat-hm-import-page.mjs
@@ -3,10 +3,12 @@ import _ from 'lodash'
 
 // 1. 拿到所有的page.json路径列表
 let pageJsonFiles = await $`find ../src/pages -name 'page.json'`
+pageJsonFiles = pageJsonFiles.stdout.split('\n')
+pageJsonFiles = pageJsonFiles.slice(0, pageJsonFiles.length - 1)
 
 // 2. 遍历所有的page.json，读取page.json里的HmImportPage组件信息并统计
 function readPageComponentName(pageId) {
-  let pageJson = fs.readFileSync(`../src/pages/${pageId}/page.json`, 'utf8');
+  let pageJson = JSON.parse(fs.readFileSync(`../src/pages/${pageId}/page.json`, 'utf8'));
   return _.camelCase(pageJson.component.name);
 }
 
@@ -14,7 +16,7 @@ function statOne(pageJson) {
   let pageStat = {};
   pageJson.topics.forEach(topic => {
     if (topic.component.name === 'HmImportPage') {
-      let pageId = topic.note.props['page-id'] || topic.note.props.pageId
+      let pageId = topic.note.props['page-id'].value.value;
       pageStat[pageId] = readPageComponentName(pageId);
     }
 
@@ -27,8 +29,8 @@ function statOne(pageJson) {
 
 let stat = {}
 pageJsonFiles.forEach((pageJsonFile) => {
-  let pageJson = fs.readFileSync(pageJsonFile, 'utf8');
+  let pageJson = JSON.parse(fs.readFileSync(pageJsonFile, 'utf8'));
   stat[pageJson.component.id] =  statOne(pageJson);
 })
 
-fs.writeFileSync('../docs/stat-hm-import-page.json', { encoding: 'utf8', mode: 'w+'})
+fs.writeFileSync('../docs/stat-hm-import-page.json', JSON.stringify(stat, null, 2), { encoding: 'utf8', flag: 'w+'})
