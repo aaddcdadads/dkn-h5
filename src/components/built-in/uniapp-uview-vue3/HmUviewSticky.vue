814c0d49ab54b64e4a6c14ee01c5af1ac39afbfa
肖君安 <xiaojunan@haomo-studio.com>
2022-09-28 14:28:33 +0800

    fix:修改吸顶

--- src/components/built-in/uniapp-uview-vue3/HmUviewSticky.vue
+++ src/components/built-in/uniapp-uview-vue3/HmUviewSticky.vue
@@ -1,53 +1,56 @@
 <template>
-    <u-sticky :offsetTop="offsetTop" :customNavHeight="customNavHeight" :disabled="disabled" :bgColor="bgColor"
-      :zIndex="zIndex">
-      <slot name="stickyDiv">
-      </slot>
-    </u-sticky>
-  </template>
-  <script>
-  export default {
-    name:"HmUviewSticky",
-    props: {
-      /**
-       * 吸顶距离rpx
-       */
-      offsetTop: {
-        type: String,
-        default: "0"
-      },
-      /**
-       * 导航栏高度
-       */
-      customNavHeight: {
-        type: String,
-        default: "0"
-      },
-      /**
-       * 是否禁用吸顶
-       */
-      disabled: {
-        type: Boolean,
-        default: false
-      },
-      /**
-       * 背景颜色
-       * @type Color
-       */
-      bgColor: {
-        type: String,
-        default: "#ffffff"
-      },
-      /**
-       * 吸顶层级
-       */
-      zIndex: {
-        type: String,
-        default: "1"
-      }
+  <u-sticky 
+  :offset-top="offsetTop" 
+  :h5-nav-height="customNavHeight" 
+  :enable="enable" 
+  :bg-color="bgColor"
+  :z-index="zIndex">
+    <slot name="stickyDiv">
+    </slot>
+  </u-sticky>
+</template>
+<script>
+export default {
+  name:"HmUviewSticky",
+  props: {
+    /**
+     * 吸顶距离rpx
+     */
+    offsetTop: {
+      type: String,
+      default: "0"
+    },
+    /**
+     * 导航栏高度
+     */
+    customNavHeight: {
+      type: String,
+      default: "0"
+    },
+    /**
+     * 是否开启吸顶
+     */
+     enable: {
+      type: Boolean,
+      default: true
+    },
+    /**
+     * 背景颜色
+     * @type Color
+     */
+    bgColor: {
+      type: String,
+      default: "#ffffff"
+    },
+    /**
+     * 吸顶层级
+     */
+    zIndex: {
+      type: String,
+      default: "1"
     }
   }
-  </script>
-  <style scoped>
-  
-  </style>
+}
+</script>
+<style lang="less" scoped>
+</style>
