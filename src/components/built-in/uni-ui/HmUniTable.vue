4bb65c8eadeb49f24e42383f56f9568abc4161e8
胡小根 <hxg@haomo-studio.com>
2022-10-10 12:12:44 +0800

    fix: fix HmUniTable

--- src/components/built-in/uni-ui/HmUniTable.vue
+++ src/components/built-in/uni-ui/HmUniTable.vue
@@ -48,7 +48,7 @@ export default {
       props: {
         vNode: [Array, String, Object, Number]
       },
-      render (h) {
+      render () {
         if (typeof this.vNode === 'object') {
           return this.vNode
         } else {
