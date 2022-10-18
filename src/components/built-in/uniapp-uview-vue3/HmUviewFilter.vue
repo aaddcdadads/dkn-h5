a0ba368572cb74c9e604190db2795e372eca260a
肖禹鑫 <xiaoyuxin@haomo-studio.com>
2022-10-18 15:11:34 +0800

    修改Filter

--- src/components/built-in/uniapp-uview-vue3/HmUviewFilter.vue
+++ src/components/built-in/uniapp-uview-vue3/HmUviewFilter.vue
@@ -12,8 +12,10 @@
         @change="change"
       ></u-input>
     </u-col>
-    <u-col span="2" class="search__text-div" @click="search">
-      搜索
+    <u-col span="2" class="search__text-div" >
+      <view @click="search">
+        搜索
+      </view>
     </u-col>
   </u-row>
   <u-row class="sort">
@@ -258,6 +260,7 @@ export default {
     },
     search(){
       this.$emit("search", {
+        key:this.value,
         filter: this.getFilterValues(),
         sort: this.getSortValues()
       })
