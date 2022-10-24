8d243226dbfb1e929592cbf099b302c6bfee22b4
肖君安 <xiaojunan@haomo-studio.com>
2022-10-24 10:55:46 +0800

    fix:添加uni-haomo部署脚本

--- built-in-deploy.sh
+++ built-in-deploy.sh
@@ -23,3 +23,8 @@ rsync -avz src/components/built-in/third-party haomo@192.168.1.7:/data/docker/de
 rsync -avz src/components/built-in/third-party haomo@192.168.1.7:/data/docker/design-cloud.stage/files/built-in
 rsync -avz src/components/built-in/third-party haomo@192.168.1.7:/data/docker/design-cloud.dev/files/built-in
 rsync -avz src/components/built-in/third-party haomo@192.168.1.8:/data/docker/design-cloud.dev/files/built-in
+
+rsync -avz src/components/built-in/uniapp-haomo haomo@192.168.1.7:/data/docker/design-cloud/files/built-in
+rsync -avz src/components/built-in/uniapp-haomo haomo@192.168.1.7:/data/docker/design-cloud.stage/files/built-in
+rsync -avz src/components/built-in/uniapp-haomo haomo@192.168.1.7:/data/docker/design-cloud.dev/files/built-in
+rsync -avz src/components/built-in/uniapp-haomo haomo@192.168.1.8:/data/docker/design-cloud.dev/files/built-in
