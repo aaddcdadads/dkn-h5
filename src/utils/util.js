a3c916ebd06d4089dc26b34559230061428dc6de
胡小根 <hxg@haomo-studio.com>
2022-10-29 10:48:11 +0800

    feat: 将lodash从util中移除

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,4 +1,14 @@
-import _ from 'lodash';
+/**
+ * 转换成小写驼峰
+ */
+ export function toCamelCase (str) {
+	let STR = str.toLowerCase()
+		.trim()
+		.split(/[ -_]/g)
+		.map(word => word.replace(word[0], word[0].toString().toUpperCase()))
+		.join('');
+	return STR.replace(STR[0], STR[0].toLowerCase());
+};
 
 export function convertCssToVueStyle(css) {
   if (!css) {
@@ -9,39 +19,13 @@ export function convertCssToVueStyle(css) {
 
   let items = css.split(';')
   let style = {}
-  _.each(items, item => {
-    let cssItem = _.trim(item)
+  items.forEach(item => {
+    let cssItem = item.trim()
     if (!cssItem) {
       return;
     }
 
-    style[_.camelCase(_.trim(cssItem.split(':')[0]))] = _.trim(cssItem.split(':')[1])
+    style[toCamelCase(cssItem.split(':')[0].trim())] = cssItem.split(':')[1].trim()
   })
   return style;
 }
-
-export function getAntTableFlattenedColumns(columns) {
-  let self = this;
-  if (typeof columns === 'undefined') {
-    columns = this.columns;
-  }
-  let flattenedColumns = JSON.parse(JSON.stringify(columns))
-  _.each(columns, column => {
-    if (column.children && column.children.length > 0) {
-      flattenedColumns = _.concat(flattenedColumns, self.getFlattenedColumns(column.children))
-    }
-  })
-  return flattenedColumns;
-}
-
-/**
- * 转换成小写驼峰
- */
-export function toCamelCase (str) {
-	let STR = str.toLowerCase()
-		.trim()
-		.split(/[ -_]/g)
-		.map(word => word.replace(word[0], word[0].toString().toUpperCase()))
-		.join('');
-	return STR.replace(STR[0], STR[0].toLowerCase());
-};
