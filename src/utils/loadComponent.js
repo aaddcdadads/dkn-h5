325af09a59ac3d3bd7495a1850aee6606a0f3e79
胡小根 <hxg@haomo-studio.com>
2022-10-30 17:42:21 +0800

    fix: fix loadComponents

--- src/utils/loadComponent.js
+++ src/utils/loadComponent.js
@@ -7,7 +7,8 @@ export default {
 	install(app ,options){
     console.log(`modules: `, JSON.stringify(modules, null, 2));
     // //根据modules加载所有组件
-    modules.forEach((module, key) => {
+    Object.keys(modules).forEach((key) => {
+      let module = modules[key];
       let arr = key.split("/");
       let lastName = arr[arr.length - 1];
       let componentName = lastName == "index.vue" ? 
