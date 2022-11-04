b042c2f2732a1b6d3b03a4140395db1344f9a6d2
肖君安 <xiaojunan@haomo-studio.com>
2022-11-04 15:29:25 +0800

    fix:修改对话框

--- src/components/built-in/uniapp-uview-vue3/HmUviewModal.vue
+++ src/components/built-in/uniapp-uview-vue3/HmUviewModal.vue
@@ -10,12 +10,12 @@
     :confirm-color="confirmColor"
     :cancel-text="cancelText"
     :cancel-color="cancelColor"
-		:mask-close-able="maskCloseAble"
-    :border-radius="borderRadius"
+    :mask-close-able="maskCloseAble"
+    :border-radius="cBorderRadius"
     :width="width"
     @confirm="onConfirm"
     @cancel="onCancel"
-		@close="close"
+    @close="close"
   >
     <slot />
   </u-modal>
@@ -24,6 +24,7 @@
 <script>
 export default {
   name: "HmUviewModal",
+  options: { styleIsolation: "shared" }, //解决/deep/不生效
   components: {},
   props: {
     /**
@@ -32,35 +33,35 @@ export default {
      */
     visible: {
       type: Boolean,
-      default: false,
+      default: true
     },
     /**
      * 显示标题
      */
     showTitle: {
       type: Boolean,
-      default: true,
+      default: true
     },
     /**
      * 对话框标题
      */
     title: {
       type: String,
-      default: "标题:对话框",
+      default: "标题:对话框"
     },
     /**
      * 显示确认按钮
      */
     showConfirmBtn: {
       type: Boolean,
-      default: true,
+      default: true
     },
     /**
      * 确认文字
      */
     confirmText: {
       type: String,
-      default: "确认",
+      default: "确认"
     },
     /**
      * 确认颜色
@@ -68,21 +69,21 @@ export default {
      */
     confirmColor: {
       type: String,
-      default: "#000000",
+      default: "#000000"
     },
     /**
      * 显示取消按钮
      */
     showCancelBtn: {
       type: Boolean,
-      default: true,
+      default: true
     },
     /**
      * 取消文字
      */
     cancelText: {
       type: String,
-      default: "取消",
+      default: "取消"
     },
     /**
      * 取消颜色
@@ -90,61 +91,67 @@ export default {
      */
     cancelColor: {
       type: String,
-      default: "#000000",
+      default: "#000000"
     },
     /**
      * 显示关闭按钮
      */
-     showCloseIcon: {
+    showCloseIcon: {
       type: Boolean,
-      default: true,
+      default: true
     },
     /**
      * 圆角
      */
     borderRadius: {
       type: String,
+      default: "12px"
     },
     /**
      * 宽度
      */
     width: {
       type: String,
-      default: "300px",
+      default: "300px"
     },
     /**
      * 高度
      */
     height: {
       type: String,
-      default: "150px",
+      default: "150px"
     },
     /**
      * 点击遮罩关闭
      */
-    maskCloseAble:{
+    maskCloseAble: {
       type: Boolean,
-      default: false,
+      default: false
     }
   },
   data() {
     return {
+      cBorderRadius: "",
       cHeight: "150px",
-      cVisible: false,
+      cVisible: false
     };
   },
   watch: {
+    borderRadius(value) {
+      this.cBorderRadius = value;
+    },
     height(value) {
       this.cHeight = this.getCssUnit(value);
     },
     visible(value) {
       this.cVisible = !!value;
-    },
+    }
   },
   computed: {},
   mounted() {
     let self = this;
     this.cHeight = this.getCssUnit(this.height);
+    this.cBorderRadius = this.borderRadius;
     self.cVisible = !!self.visible;
   },
   methods: {
@@ -163,12 +170,15 @@ export default {
     close(e) {
       this.$emit("close", e);
       this.$emit("update:visible", false);
-    },
-  },
+    }
+  }
 };
 </script>
 <style lang="less">
 /deep/ .u-model__content {
   height: v-bind(cHeight);
+}
+/deep/ .u-mode-center-box {
+  border-radius: v-bind(cBorderRadius);
 }
 </style>
