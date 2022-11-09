11f8229ae5230d65d0d1d53cf0dbe583bbdb75d2
肖君安 <xiaojunan@haomo-studio.com>
2022-11-09 10:12:17 +0800

    fix:built-in-deploy

--- built-in-deploy.sh
+++ built-in-deploy.sh
@@ -28,3 +28,8 @@ rsync -avz src/components/built-in/uniapp-haomo haomo@192.168.1.7:/data/docker/d
 rsync -avz src/components/built-in/uniapp-haomo haomo@192.168.1.7:/data/docker/design-cloud.stage/files/built-in
 rsync -avz src/components/built-in/uniapp-haomo haomo@192.168.1.7:/data/docker/design-cloud.dev/files/built-in
 rsync -avz src/components/built-in/uniapp-haomo haomo@192.168.1.8:/data/docker/design-cloud.dev/files/built-in
+
+rsync -avz src/components/built-in/echarts-for-wx haomo@192.168.1.7:/data/docker/design-cloud/files/built-in
+rsync -avz src/components/built-in/echarts-for-wx haomo@192.168.1.7:/data/docker/design-cloud.stage/files/built-in
+rsync -avz src/components/built-in/echarts-for-wx haomo@192.168.1.7:/data/docker/design-cloud.dev/files/built-in
+rsync -avz src/components/built-in/echarts-for-wx haomo@192.168.1.8:/data/docker/design-cloud.dev/files/built-in
