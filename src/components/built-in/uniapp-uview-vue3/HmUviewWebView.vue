ad27605194f6fa2c82314b35d3a94dff988ef88c
陶宁 <taoning@haomo-studio.com>
2022-12-14 18:29:53 +0800

    add: 添加webView组件

---
+++ src/components/built-in/uniapp-uview-vue3/HmUviewWebView.vue
@@ -0,0 +1,68 @@
+<template>
+  <view class="container">
+    <web-view :src="cSrc" :width="cWidth" :height="cHeight"></web-view>
+  </view>
+</template>
+
+<script>
+export default {
+  props: {
+    /**
+     * 宽度
+     */
+    width: {
+      type: String,
+      default: '200'
+    },
+    /**
+     * 高度
+     */
+    height: {
+      type: String,
+      default: '200'
+    },
+    /**
+     * 地址
+     */
+    src: {
+      type: String,
+      default: 'https://baidu.com'
+    }
+  },
+  watch: {
+    width(value) {
+      this.cWidth = this.getCssUnit(value);
+    },
+    height(value) {
+      this.cHeight = this.getCssUnit(value);
+    },
+    src(value) {
+      this.cSrc = value
+    }
+  },
+  mounted() {
+    this.cSrc = this.src
+    this.cWidth = this.getCssUnit(this.width);
+    this.cHeight = this.getCssUnit(this.height);
+  },
+  data() {
+    return {
+      cSrc: 'https://baidu.com',
+      cWidth: "",
+      cHeight: ""
+    }
+  },
+  methods: {
+    getCssUnit(value) {
+      if (isNaN(Number(value))) {
+        return value;
+      }
+      return `${value}px`;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
