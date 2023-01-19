a85502c5bc2aff0c7f57b0147e5e1cb35f51f8a1
肖君安 <xiaojunan@haomo-studio.com>
2023-01-19 21:31:26 +0800

    fix:添加uniapp评分组件(修改)

---
+++ src/components/built-in/uniapp-haomo/HmUniappRate.vue
@@ -0,0 +1,214 @@
+<template>
+  <view class="HmUniappRate">
+    <text v-if="cLabel" class="HmUniappRate-label" :style="cLabelStyle" decode>{{ cLabel }}</text>
+    <uni-rate
+      v-model="cValue"
+      class="HmUniappRate-rate"
+      :allowHalf="allowHalf"
+      :color="color"
+      :activeColor="activeColor"
+      :size="size"
+      :max="max"
+      :margin="margin"
+      :readonly="readonly"
+      :disabledColor="disabledColor"
+      :disabled="disabled"
+      :isFill="isFill"
+      :touchable="touchable"
+      @change="rateChange"
+    />
+    <text v-if="cDisplayText.show" class="HmUniappRate-text" :style="cDisplayText.style" decode>{{
+      getDisplayText(cValue)
+    }}</text>
+  </view>
+</template>
+<script>
+export default {
+  name: "HmUniappRate",
+  props: {
+    /**
+     * 评分
+     */
+    value: {
+      type: Number,
+      default: 3.5,
+    },
+    /**
+     * 标题
+     */
+    label: {
+      type: String,
+      default: "评分",
+    },
+    /**
+     * 是否展示半星
+     */
+    allowHalf: {
+      type: Boolean,
+      default: true,
+    },
+    /**
+     * 星星的大小
+     */
+    size: {
+      type: Number,
+      default: 24,
+    },
+    /**
+     * 最小评分
+     */
+    min: {
+      type: Number,
+      default: 1,
+    },
+    /**
+     * 最大评分
+     */
+    max: {
+      type: Number,
+      default: 5,
+    },
+    /**
+     * 星星间距px
+     */
+    margin: {
+      type: Number,
+      default: 0,
+    },
+    /**
+     * 只读状态
+     */
+    readonly: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * 未选中状态
+     * @type Color
+     */
+    color: {
+      type: String,
+      default: "#ececec",
+    },
+    /**
+     * 选中状态
+     * @type Color
+     */
+    activeColor: {
+      type: String,
+      default: "#F37931",
+    },
+    /**
+     * 禁用状态
+     * @type Color
+     */
+    disabledColor: {
+      type: String,
+      default: "#ffca3e",
+    },
+    /**
+     * 禁用状态
+     */
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    /**
+     * 是否实心类型
+     */
+    isFill: {
+      type: Boolean,
+      default: true,
+    },
+    /**
+     * 支持手势滑动
+     */
+    touchable: {
+      type: Boolean,
+      default: true,
+    },
+    /**
+     * 标题样式
+     */
+    labelStyle: {
+      type: Object,
+      default: function () {
+        return {
+          width:'80rpx',
+          textAlign:'center',
+          fontFamily: "PingFangSC-Regular",
+          fontSize: "14px",
+          color: "#838395",
+          letterSpacing: "0",
+          fontWeight: "400",
+        };
+      },
+    },
+    /**
+     * 展示文字
+     */
+    displayText: {
+      type: Object,
+      default: function () {
+        return {
+          show: true,
+          format: "return value + '分'",
+          style: {
+            fontFamily: "PingFangSC-Regular",
+            fontSize: "14px",
+            color: "#F37931",
+            letterSpacing: "0",
+            fontWeight: "500",
+          },
+        };
+      },
+    },
+  },
+  watch: {
+    value(val) {
+      this.cValue = val;
+    },
+    label(val) {
+      this.cLabel = val;
+    },
+    labelStyle(val) {
+      this.cLabelStyle = val;
+    },
+    displayText(val) {
+      this.cDisplayText = val;
+    },
+  },
+  mounted() {
+    this.cValue = this.value;
+    this.cLabel = this.label;
+    this.cLabelStyle = this.labelStyle;
+    this.cDisplayText = this.displayText;
+  },
+  data() {
+    return {
+      cValue: 0,
+      cLabel: "",
+      cLabelStyle: "",
+      cDisplayText: {},
+    };
+  },
+  methods: {
+    rateChange(e) {
+      this.$emit("rateChange", e);
+    },
+    getDisplayText(e) {
+      let str = this.cDisplayText.format ? this.cDisplayText.format : 'return value';
+      let fun = new Function('value',str);
+      return fun(e);
+    },
+  },
+};
+</script>
+<style lang="less" scoped>
+.HmUniappRate {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+</style>
