7b019af431b8ff965b2ba280de08f479c4f601b3
胡小根 <hxg@haomo-studio.com>
2023-02-19 08:18:23 +0800

    chore: 引入blockDesign.js，简化main.js

--- src/main.js
+++ src/main.js
@@ -7,56 +7,17 @@ import "./static/css/index.css";
 /* #endif */
 
 import uView from './uni_modules/vk-uview-ui';
-
-// #ifdef H5
-import loadComponent from './utils/loadComponent';
-// #endif
-
 import "./mock/index.js";
 
-import {
-    map,
-    filter,
-    reduce,
-    array2tree
-} from './utils/flowUtils';
-
-import {
-    getAction,
-    downloadFile,
-    postAction,
-    deleteAction,
-    putAction,
-} from "./request/http";
+import blockDesign from "./utils/blockDesign";
 
 export function createApp() {
     const app = createSSRApp(App);
 
-    app.config.globalProperties.$getAction = getAction;
-    app.config.globalProperties.$downloadFile = downloadFile;
-    app.config.globalProperties.$postAction = postAction;
-    app.config.globalProperties.$deleteAction = deleteAction;
-    app.config.globalProperties.$putAction = putAction;
-
-    app.config.globalProperties.$map = map;
-    app.config.globalProperties.$filter = filter;
-    app.config.globalProperties.$reduce = reduce;
-    app.config.globalProperties.$array2tree = array2tree;
-
-    app.config.globalProperties.$getCssUnit = function (value) {
-        if (isNaN(Number(value))) {
-            return value;
-        }
-        return `${value}rpx`;
-    }
+    app.use(blockDesign);
 
     // app.use(message);
     app.use(uView);
-
-    //加载组件
-    // #ifdef H5
-    app.use(loadComponent);
-    // #endif
 
     return {
         app,
---
+++ src/utils/blockDesign.js
@@ -0,0 +1,51 @@
+import { defineAsyncComponent } from "vue";
+import { toCamelCase, upperFirst } from './util';
+
+import {
+  map,
+  filter,
+  reduce,
+  array2tree
+} from './utils/flowUtils';
+
+import {
+  getAction,
+  downloadFile,
+  postAction,
+  deleteAction,
+  putAction,
+} from "./request/http";
+
+// #ifdef H5
+import loadComponent from './utils/loadComponent';
+// #endif
+
+/**
+ * 注册设计云相关的组件
+ */
+export default {
+	install(app, options){
+    app.config.globalProperties.$getAction = getAction;
+    app.config.globalProperties.$downloadFile = downloadFile;
+    app.config.globalProperties.$postAction = postAction;
+    app.config.globalProperties.$deleteAction = deleteAction;
+    app.config.globalProperties.$putAction = putAction;
+
+    app.config.globalProperties.$map = map;
+    app.config.globalProperties.$filter = filter;
+    app.config.globalProperties.$reduce = reduce;
+    app.config.globalProperties.$array2tree = array2tree;
+
+    app.config.globalProperties.$getCssUnit = function (value) {
+        if (isNaN(Number(value))) {
+            return value;
+        }
+        return `${value}rpx`;
+    }
+
+    //加载组件
+    // #ifdef H5
+    app.use(loadComponent);
+    // #endif
+  }
+}
