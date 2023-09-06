a1d07d87d27407491650958f1703a30ad85ac5aa
王露雨 <wangluyu@haomo-studio.com>
2023-09-06 14:50:43 +0800

    fix:buit-in-deploy

--- built-in-deploy.sh
+++ built-in-deploy.sh
@@ -33,3 +33,14 @@ rsync -avz src/components/built-in/echarts-for-wx haomo@192.168.1.7:/data/docker
 rsync -avz src/components/built-in/echarts-for-wx haomo@192.168.1.7:/data/docker/design-cloud.stage/files/built-in
 rsync -avz src/components/built-in/echarts-for-wx haomo@192.168.1.7:/data/docker/design-cloud.dev/files/built-in
 rsync -avz src/components/built-in/echarts-for-wx haomo@192.168.1.8:/data/docker/design-cloud.dev/files/built-in
+
+rsync -avz src/components/built-in/uniapp-official haomo@192.168.1.7:/data/docker/design-cloud/files/built-in
+rsync -avz src/components/built-in/uniapp-official haomo@192.168.1.7:/data/docker/design-cloud.stage/files/built-in
+rsync -avz src/components/built-in/uniapp-official haomo@192.168.1.7:/data/docker/design-cloud.dev/files/built-in
+rsync -avz src/components/built-in/uniapp-official haomo@192.168.1.8:/data/docker/design-cloud.dev/files/built-in
+
+rsync -avz src/components/built-in/uniapp-uview1.x haomo@192.168.1.7:/data/docker/design-cloud/files/built-in
+rsync -avz src/components/built-in/uniapp-uview1.x haomo@192.168.1.7:/data/docker/design-cloud.stage/files/built-in
+rsync -avz src/components/built-in/uniapp-uview1.x haomo@192.168.1.7:/data/docker/design-cloud.dev/files/built-in
+rsync -avz src/components/built-in/uniapp-uview1.x haomo@192.168.1.8:/data/docker/design-cloud.dev/files/built-in
+
