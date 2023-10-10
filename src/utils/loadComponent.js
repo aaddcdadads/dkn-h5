4ac18a28829273ad7ebbe933a3844c786bfd57aa
胡小根 <hxg@haomo-studio.com>
2023-10-10 16:46:52 +0800

    feat: 增加加载uni-ui为动态组件的支持

--- src/utils/loadComponent.js
+++ src/utils/loadComponent.js
@@ -1,5 +1,6 @@
 import { defineAsyncComponent } from "vue";
 export const modules = import.meta.glob("../components/**/**.vue");
+export const uniUiModules = import.meta.glob("../../node_modules/@dcloudio/uni-ui/lib/**/**.vue");
 
 import { toCamelCase, upperFirst } from './util';
 
@@ -7,6 +8,16 @@ export default {
 	install(app, options){
     // //根据modules加载所有组件
     Object.keys(modules).forEach((key) => {
+      let module = modules[key];
+      let arr = key.split("/");
+      let lastName = arr[arr.length - 1];
+      let componentName = lastName == "index.vue" ? 
+        arr[arr.length - 2] : lastName.split(".vue")[0]
+      app.component(upperFirst(toCamelCase(componentName)), defineAsyncComponent(module));
+    })
+
+    // 加载uni-ui的所有组件
+    Object.keys(uniUiModules).forEach((key) => {
       let module = modules[key];
       let arr = key.split("/");
       let lastName = arr[arr.length - 1];
