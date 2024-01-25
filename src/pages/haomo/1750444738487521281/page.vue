f6842d29d399c0c50a19e68ae1e9b4865e9720a3
胡小根 <hxg@haomo.com>
2024-01-25 17:11:28 +0800

    feat: NUSEP网站-更新设计云页面清单

--- src/pages/haomo/1750444738487521281/page.vue
+++ src/pages/haomo/1750444738487521281/page.vue
@@ -1,28 +1,48 @@
 <template>
   <view class="page card">
     <view class="page-wrapper">
-        <view class="ele-wrapper ele-wrapper-ff006dbd-abf0-427a-9076-d34d31098de7">
-      <hm-uview-bg-card width="100%" height="100%" border-radius="" box-shadow-v-shadow="" box-shadow-blur="" class="ele-ff006dbd-abf0-427a-9076-d34d31098de7"> 
-            <view class="ele-wrapper ele-wrapper-4277c03e-3f10-4ac4-a323-2987eeb1c6a9">
-      <hm-uview-bg-card width="100%" height="100%" border-radius="" box-shadow-v-shadow="" box-shadow-blur=""> 
-            <view class="ele-wrapper ele-wrapper-5dd2074f-29d9-44b9-a829-c6b997d9652b">
-      <hm-uview-text text="资料填写" :text-style='{"fontWeight":700}'> 
-       
-    </hm-uview-text>
-    </view> 
-    </hm-uview-bg-card>
-    </view> 
-    </hm-uview-bg-card>
-    </view>
+      <view
+        class="ele-wrapper ele-wrapper-ff006dbd-abf0-427a-9076-d34d31098de7"
+      >
+        <hm-uview-bg-card
+          width="100%"
+          height="100%"
+          border-radius=""
+          box-shadow-v-shadow=""
+          box-shadow-blur=""
+          class="ele-ff006dbd-abf0-427a-9076-d34d31098de7"
+        >
+          <view
+            class="ele-wrapper ele-wrapper-4277c03e-3f10-4ac4-a323-2987eeb1c6a9"
+          >
+            <hm-uview-bg-card
+              width="100%"
+              height="100%"
+              border-radius=""
+              box-shadow-v-shadow=""
+              box-shadow-blur=""
+            >
+              <view
+                class="ele-wrapper ele-wrapper-5dd2074f-29d9-44b9-a829-c6b997d9652b"
+              >
+                <hm-uview-text
+                  text="资料填写"
+                  :text-style="{ fontWeight: 700 }"
+                >
+                </hm-uview-text>
+              </view>
+            </hm-uview-bg-card>
+          </view>
+        </hm-uview-bg-card>
+      </view>
     </view>
   </view>
 </template>
 
 <script>
-import { h } from 'vue';
+import { h } from "vue";
 import HmUviewBgCard from "/@/components/built-in/uniapp-uview-vue3/HmUviewBgCard.vue";
 import HmUviewText from "/@/components/built-in/uniapp-uview-vue3/HmUviewText.vue";
-
 
 export default {
   name: "RegistrationForm",
@@ -35,21 +55,16 @@ export default {
     let self = this;
     return {};
   },
-  watch: {
-  },
-  methods:{
-
-  }
+  watch: {},
+  methods: {},
 };
 </script>
 
 <style lang="less" scoped>
 .page {
-  
 }
 
 .ele-wrapper-ff006dbd-abf0-427a-9076-d34d31098de7 {
-  width:100%;
+  width: 100%;
 }
-
 </style>
