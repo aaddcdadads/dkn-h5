0a6950165dff8da02311248d520578aed045f13f
胡小根 <hxg@haomo.com>
2024-01-25 20:01:40 +0800

    feat: NUSEP网站-更新设计云页面清单

--- src/pages/haomo/1750443401116913665/page.vue
+++ src/pages/haomo/1750443401116913665/page.vue
@@ -337,6 +337,7 @@ export default {
 
 .ele-wrapper-ea222d02-9ba2-44be-b94e-799272b9be37 {
   width: 80%;
+  background: rbg(245, 245, 245);
 }
 
 .ele-wrapper-eab88e13-fecb-4ae5-b3c3-ac874b9d8cc9 {
