4392dd5ea46724d0bfeeda459cba5ccb59cf6643
胡小根 <hxg@haomo.com>
2024-01-25 17:14:54 +0800

    feat: NUSEP网站-更新设计云页面清单

--- src/pages/haomo/1750443401116913665/page.vue
+++ src/pages/haomo/1750443401116913665/page.vue
@@ -10,16 +10,6 @@
           class="ele-c57e9abe-e655-4515-acc3-575489ca1e33"
         >
           <view
-            class="ele-wrapper ele-wrapper-4cc93495-0f38-4da7-898e-c8573db6c6a7"
-          >
-            <hm-uview-icon
-              name="left"
-              label=""
-              class="ele-4cc93495-0f38-4da7-898e-c8573db6c6a7"
-            >
-            </hm-uview-icon>
-          </view>
-          <view
             class="ele-wrapper ele-wrapper-815e922a-f221-499a-95b7-5efffa64730c"
           >
             <hm-uview-text
@@ -37,14 +27,12 @@
 <script>
 import { h } from "vue";
 import HmUviewBgCard from "/@/components/built-in/uniapp-uview-vue3/HmUviewBgCard.vue";
-import HmUviewIcon from "/@/components/built-in/uniapp-uview-vue3/HmUviewIcon.vue";
 import HmUviewText from "/@/components/built-in/uniapp-uview-vue3/HmUviewText.vue";
 
 export default {
   name: "EventRegistrationInformation",
   components: {
     HmUviewBgCard,
-    HmUviewIcon,
     HmUviewText,
   },
   options: { styleIsolation: "shared" },
@@ -64,10 +52,6 @@ export default {
 .ele-wrapper-c57e9abe-e655-4515-acc3-575489ca1e33 {
   width: 100%;
   height: 100%;
-}
-
-.ele-wrapper-4cc93495-0f38-4da7-898e-c8573db6c6a7 {
-  margin-left: 10px;
 }
 
 .ele-wrapper-815e922a-f221-499a-95b7-5efffa64730c {
