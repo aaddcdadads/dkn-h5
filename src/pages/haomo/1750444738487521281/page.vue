d602d141ad6f79099317ba499f31576f778407c1
胡小根 <hxg@haomo.com>
2024-01-25 17:15:58 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

--- src/pages/haomo/1750444738487521281/page.vue
+++ src/pages/haomo/1750444738487521281/page.vue
@@ -1,33 +1,24 @@
 <template>
   <view class="page card">
     <view class="page-wrapper">
-      <view
-        class="ele-wrapper ele-wrapper-ff006dbd-abf0-427a-9076-d34d31098de7"
-      >
-        <hm-uview-bg-card
-          width="100%"
-          height="100%"
-          border-radius=""
-          box-shadow-v-shadow=""
-          box-shadow-blur=""
-          class="ele-ff006dbd-abf0-427a-9076-d34d31098de7"
-        >
-          <view
-            class="ele-wrapper ele-wrapper-e4e2b49b-b097-4a65-a356-d9d95daa618b"
-          >
-            <hm-uview-text text="资料填写" :text-style="{ fontWeight: 600 }">
-            </hm-uview-text>
-          </view>
-        </hm-uview-bg-card>
-      </view>
+        <view class="ele-wrapper ele-wrapper-ff006dbd-abf0-427a-9076-d34d31098de7">
+      <hm-uview-bg-card width="100%" height="100%" border-radius="" box-shadow-v-shadow="" box-shadow-blur="" class="ele-ff006dbd-abf0-427a-9076-d34d31098de7"> 
+            <view class="ele-wrapper ele-wrapper-e4e2b49b-b097-4a65-a356-d9d95daa618b">
+      <hm-uview-text text="资料填写" :text-style='{"fontWeight":700}'> 
+       
+    </hm-uview-text>
+    </view> 
+    </hm-uview-bg-card>
+    </view>
     </view>
   </view>
 </template>
 
 <script>
-import { h } from "vue";
+import { h } from 'vue';
 import HmUviewBgCard from "/@/components/built-in/uniapp-uview-vue3/HmUviewBgCard.vue";
 import HmUviewText from "/@/components/built-in/uniapp-uview-vue3/HmUviewText.vue";
+
 
 export default {
   name: "RegistrationForm",
@@ -40,16 +31,21 @@ export default {
     let self = this;
     return {};
   },
-  watch: {},
-  methods: {},
+  watch: {
+  },
+  methods:{
+
+  }
 };
 </script>
 
 <style lang="less" scoped>
 .page {
+  
 }
 
 .ele-wrapper-ff006dbd-abf0-427a-9076-d34d31098de7 {
-  width: 100%;
+  width:100%;
 }
+
 </style>
