ddb36606c360f1c9e177d017edaedb10cc577735
胡小根 <hxg@haomo.com>
2024-01-25 19:58:36 +0800

    feat: NUSEP网站-更新设计云页面清单

--- src/pages/haomo/1750443401116913665/page.vue
+++ src/pages/haomo/1750443401116913665/page.vue
@@ -75,7 +75,6 @@
                       width="100%"
                       height="40"
                       :text-align="'center'"
-                      padding=""
                       box-shadow-v-shadow=""
                       box-shadow-blur=""
                       class="ele-3594e500-632d-42e8-b3b9-d485a0f784a5"
@@ -99,6 +98,111 @@
                           icon=""
                           right-icon=""
                           class="ele-ea222d02-9ba2-44be-b94e-799272b9be37"
+                        >
+                        </hm-uview-field>
+                      </view>
+                    </hm-uview-bg-card>
+                  </view>
+                  <view
+                    class="ele-wrapper ele-wrapper-eab88e13-fecb-4ae5-b3c3-ac874b9d8cc9"
+                  >
+                    <hm-uview-bg-card
+                      width="100%"
+                      height="40"
+                      :text-align="'center'"
+                      box-shadow-v-shadow=""
+                      box-shadow-blur=""
+                      class="ele-eab88e13-fecb-4ae5-b3c3-ac874b9d8cc9"
+                    >
+                      <view
+                        class="ele-wrapper ele-wrapper-d166dfa0-a20d-4d76-a53e-7fffb0834c9f"
+                      >
+                        <hm-uview-text
+                          font-size="13px"
+                          class="ele-d166dfa0-a20d-4d76-a53e-7fffb0834c9f"
+                        >
+                        </hm-uview-text>
+                      </view>
+                      <view
+                        class="ele-wrapper ele-wrapper-c5bfde68-4a10-445e-b173-b62b3921231b"
+                      >
+                        <hm-uview-field
+                          value=""
+                          label=""
+                          label-width="0"
+                          icon=""
+                          right-icon=""
+                          class="ele-c5bfde68-4a10-445e-b173-b62b3921231b"
+                        >
+                        </hm-uview-field>
+                      </view>
+                    </hm-uview-bg-card>
+                  </view>
+                  <view
+                    class="ele-wrapper ele-wrapper-15f1e19b-dbc6-4869-a68e-a5c9ae58fad0"
+                  >
+                    <hm-uview-bg-card
+                      width="100%"
+                      height="40"
+                      :text-align="'center'"
+                      box-shadow-v-shadow=""
+                      box-shadow-blur=""
+                      class="ele-15f1e19b-dbc6-4869-a68e-a5c9ae58fad0"
+                    >
+                      <view
+                        class="ele-wrapper ele-wrapper-37f7c953-8895-4c12-80d8-e4bbd27b84a2"
+                      >
+                        <hm-uview-text
+                          font-size="13px"
+                          class="ele-37f7c953-8895-4c12-80d8-e4bbd27b84a2"
+                        >
+                        </hm-uview-text>
+                      </view>
+                      <view
+                        class="ele-wrapper ele-wrapper-e3774afb-14a0-4b23-adda-2624414eb025"
+                      >
+                        <hm-uview-field
+                          value=""
+                          label=""
+                          label-width="0"
+                          icon=""
+                          right-icon=""
+                          class="ele-e3774afb-14a0-4b23-adda-2624414eb025"
+                        >
+                        </hm-uview-field>
+                      </view>
+                    </hm-uview-bg-card>
+                  </view>
+                  <view
+                    class="ele-wrapper ele-wrapper-ec8bdb37-8258-4daf-b83b-30ebc2a70f11"
+                  >
+                    <hm-uview-bg-card
+                      width="100%"
+                      height="40"
+                      :text-align="'center'"
+                      box-shadow-v-shadow=""
+                      box-shadow-blur=""
+                      class="ele-ec8bdb37-8258-4daf-b83b-30ebc2a70f11"
+                    >
+                      <view
+                        class="ele-wrapper ele-wrapper-3abd57d9-0713-43df-a2c8-ef7c647ae545"
+                      >
+                        <hm-uview-text
+                          font-size="13px"
+                          class="ele-3abd57d9-0713-43df-a2c8-ef7c647ae545"
+                        >
+                        </hm-uview-text>
+                      </view>
+                      <view
+                        class="ele-wrapper ele-wrapper-9a4984ec-97fb-465a-9b6b-8c645b5eab71"
+                      >
+                        <hm-uview-field
+                          value=""
+                          label=""
+                          label-width="0"
+                          icon=""
+                          right-icon=""
+                          class="ele-9a4984ec-97fb-465a-9b6b-8c645b5eab71"
                         >
                         </hm-uview-field>
                       </view>
@@ -168,6 +272,15 @@ export default {
       "ea222d02-9ba2-44be-b94e-799272b9be37": {
         value: "",
       },
+      "c5bfde68-4a10-445e-b173-b62b3921231b": {
+        value: "",
+      },
+      "e3774afb-14a0-4b23-adda-2624414eb025": {
+        value: "",
+      },
+      "9a4984ec-97fb-465a-9b6b-8c645b5eab71": {
+        value: "",
+      },
     };
   },
   watch: {},
@@ -218,6 +331,48 @@ export default {
 }
 
 .ele-wrapper-ea222d02-9ba2-44be-b94e-799272b9be37 {
+  width: 80%;
+}
+
+.ele-wrapper-eab88e13-fecb-4ae5-b3c3-ac874b9d8cc9 {
+  width: 100%;
+  display: flex;
+  align-items: center;
+}
+
+.ele-wrapper-d166dfa0-a20d-4d76-a53e-7fffb0834c9f {
+  width: 20%;
+}
+
+.ele-wrapper-c5bfde68-4a10-445e-b173-b62b3921231b {
+  width: 80%;
+}
+
+.ele-wrapper-15f1e19b-dbc6-4869-a68e-a5c9ae58fad0 {
+  width: 100%;
+  display: flex;
+  align-items: center;
+}
+
+.ele-wrapper-37f7c953-8895-4c12-80d8-e4bbd27b84a2 {
+  width: 20%;
+}
+
+.ele-wrapper-e3774afb-14a0-4b23-adda-2624414eb025 {
+  width: 80%;
+}
+
+.ele-wrapper-ec8bdb37-8258-4daf-b83b-30ebc2a70f11 {
+  width: 100%;
+  display: flex;
+  align-items: center;
+}
+
+.ele-wrapper-3abd57d9-0713-43df-a2c8-ef7c647ae545 {
+  width: 20%;
+}
+
+.ele-wrapper-9a4984ec-97fb-465a-9b6b-8c645b5eab71 {
   width: 80%;
 }
 
