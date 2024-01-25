5d70a71cb4082e6b57648f02c88d7027186a2234
胡小根 <hxg@haomo.com>
2024-01-25 18:12:34 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

--- src/pages/haomo/1750444738487521281/page.vue
+++ src/pages/haomo/1750444738487521281/page.vue
@@ -18,6 +18,26 @@
             <hm-uview-text text="资料填写" :text-style="{ fontWeight: 700 }">
             </hm-uview-text>
           </view>
+          <view
+            class="ele-wrapper ele-wrapper-7927eb64-d6cf-4dda-8938-a15bb62187d2"
+          >
+            <hm-uview-bg-card
+              width="100%"
+              height="100%"
+              border-radius=""
+              box-shadow-v-shadow=""
+              box-shadow-blur=""
+              box-shadow-spread=""
+              class="ele-7927eb64-d6cf-4dda-8938-a15bb62187d2"
+            >
+              <view
+                class="ele-wrapper ele-wrapper-37f4c2a6-8b23-4459-bbbe-1b24e423d813"
+              >
+                <hm-uview-field value="" label="" label-width="">
+                </hm-uview-field>
+              </view>
+            </hm-uview-bg-card>
+          </view>
         </hm-uview-bg-card>
       </view>
     </view>
@@ -28,17 +48,23 @@
 import { h } from "vue";
 import HmUviewBgCard from "/@/components/built-in/uniapp-uview-vue3/HmUviewBgCard.vue";
 import HmUviewText from "/@/components/built-in/uniapp-uview-vue3/HmUviewText.vue";
+import HmUviewField from "/@/components/built-in/uniapp-uview-vue3/HmUviewField.vue";
 
 export default {
   name: "RegistrationForm",
   components: {
     HmUviewBgCard,
     HmUviewText,
+    HmUviewField,
   },
   options: { styleIsolation: "shared" },
   data() {
     let self = this;
-    return {};
+    return {
+      "37f4c2a6-8b23-4459-bbbe-1b24e423d813": {
+        value: "",
+      },
+    };
   },
   watch: {},
   methods: {},
@@ -50,6 +76,10 @@ export default {
 }
 
 .ele-wrapper-ff006dbd-abf0-427a-9076-d34d31098de7 {
+  width: 100%;
+}
+
+.ele-wrapper-7927eb64-d6cf-4dda-8938-a15bb62187d2 {
   width: 100%;
 }
 </style>
