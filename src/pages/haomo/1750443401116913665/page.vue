a4a1fda3a5c9d2e01b17912728e72777d32970be
胡小根 <hxg@haomo.com>
2024-01-25 17:04:16 +0800

    feat: NUSEP网站-更新设计云页面清单

--- src/pages/haomo/1750443401116913665/page.vue
+++ src/pages/haomo/1750443401116913665/page.vue
@@ -6,9 +6,14 @@
       >
         <hm-uview-bg-card
           width="100%"
-          height="50"
+          height="100%"
           class="ele-c57e9abe-e655-4515-acc3-575489ca1e33"
         >
+          <view
+            class="ele-wrapper ele-wrapper-a3bb82dc-69fc-433d-bbb0-5ddb9964155e"
+          >
+            <hm-icon :size="20"> </hm-icon>
+          </view>
         </hm-uview-bg-card>
       </view>
     </view>
@@ -18,11 +23,13 @@
 <script>
 import { h } from "vue";
 import HmUviewBgCard from "/@/components/built-in/uniapp-uview-vue3/HmUviewBgCard.vue";
+import HmIcon from "/@/components/built-in/uniapp/HmIcon.vue";
 
 export default {
   name: "EventRegistrationInformation",
   components: {
     HmUviewBgCard,
+    HmIcon,
   },
   options: { styleIsolation: "shared" },
   data() {
