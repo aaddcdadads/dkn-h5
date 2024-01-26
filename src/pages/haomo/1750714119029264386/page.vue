b37ffeb06c57ad5d986bf1ecc6bbfc3b00a85be0
胡小根 <hxg@haomo.com>
2024-01-26 10:55:49 +0800

    feat: NUSEP网站-更新设计云页面清单

---
+++ src/pages/haomo/1750714119029264386/page.vue
@@ -0,0 +1,26 @@
+<template>
+  <view class="page card">
+    <view class="page-wrapper"> </view>
+  </view>
+</template>
+
+<script>
+import { h } from "vue";
+
+export default {
+  name: "ActivityRegistrationPage",
+  components: {},
+  options: { styleIsolation: "shared" },
+  data() {
+    let self = this;
+    return {};
+  },
+  watch: {},
+  methods: {},
+};
+</script>
+
+<style lang="less" scoped>
+.page {
+}
+</style>
