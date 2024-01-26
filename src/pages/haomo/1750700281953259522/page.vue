4946123d7b74ed02f8636a47853ddb17e81060f9
胡小根 <hxg@haomo.com>
2024-01-26 16:36:50 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

--- src/pages/haomo/1750700281953259522/page.vue
+++ src/pages/haomo/1750700281953259522/page.vue
@@ -364,11 +364,11 @@ export default {
 }
 
 .ele-wrapper-788913aa-482e-4c28-855d-fdb18b0c1429 {
-  width: 25%;
+  width: 32%;
 }
 
 .ele-wrapper-ea222d02-9ba2-44be-b94e-799272b9be37 {
-  width: 70%;
+  width: 67%;
   height: 45px;
   /deep/.u-border-bottom {
     background-color: rgb(240, 240, 240);
@@ -384,11 +384,11 @@ export default {
 }
 
 .ele-wrapper-d166dfa0-a20d-4d76-a53e-7fffb0834c9f {
-  width: 25%;
+  width: 32%;
 }
 
 .ele-wrapper-c5bfde68-4a10-445e-b173-b62b3921231b {
-  width: 70%;
+  width: 67%;
   /deep/.u-border-bottom {
     background-color: rgb(240, 240, 240);
     border-radius: 5px;
@@ -404,11 +404,11 @@ export default {
 }
 
 .ele-wrapper-37f7c953-8895-4c12-80d8-e4bbd27b84a2 {
-  width: 25%;
+  width: 32%;
 }
 
 .ele-wrapper-e3774afb-14a0-4b23-adda-2624414eb025 {
-  width: 70%;
+  width: 67%;
   /deep/.u-border-bottom {
     background-color: rgb(240, 240, 240);
     border-radius: 5px;
@@ -429,11 +429,11 @@ export default {
 }
 
 .ele-wrapper-871e304d-82cd-4165-96f5-cd60704fac71 {
-  width: 25%;
+  width: 32%;
 }
 
 .ele-wrapper-6bdb201e-d452-4e15-b72f-70c229fbd237 {
-  width: 70%;
+  width: 67%;
   /deep/.u-border-bottom {
     background-color: rgb(240, 240, 240);
     border-radius: 5px;
