ac0e76accca634c17ca27c0ea4d78b7328cae505
胡小根 <hxg@haomo.com>
2024-01-26 10:36:23 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

--- src/pages/haomo/1750444738487521281/page.vue
+++ src/pages/haomo/1750444738487521281/page.vue
@@ -1,34 +1,60 @@
 <template>
   <view class="page card">
     <view class="page-wrapper">
-        <view class="ele-wrapper ele-wrapper-ff006dbd-abf0-427a-9076-d34d31098de7">
-      <hm-uview-bg-card width="100%" height="100%" border-radius="" box-shadow-v-shadow="" box-shadow-blur="" class="ele-ff006dbd-abf0-427a-9076-d34d31098de7"> 
-            <view class="ele-wrapper ele-wrapper-e4e2b49b-b097-4a65-a356-d9d95daa618b">
-      <hm-uview-text text="资料填写" :text-style='{"fontWeight":700}'> 
-       
-    </hm-uview-text>
-    </view> 
-            <view class="ele-wrapper ele-wrapper-7927eb64-d6cf-4dda-8938-a15bb62187d2">
-      <hm-uview-bg-card width="100%" height="100%" border-radius="" box-shadow-v-shadow="" box-shadow-blur="" box-shadow-spread="" class="ele-7927eb64-d6cf-4dda-8938-a15bb62187d2"> 
-            <view class="ele-wrapper ele-wrapper-8b981cf2-ae14-4b8c-9f72-4cd6c41423a6">
-      <information-input-box :left-src='["\"https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/touxiang.png\"","\"https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/touxiang1.png\""]' value="1212"> 
-       
-    </information-input-box>
-    </view> 
-    </hm-uview-bg-card>
-    </view> 
-    </hm-uview-bg-card>
-    </view>
+      <view
+        class="ele-wrapper ele-wrapper-ff006dbd-abf0-427a-9076-d34d31098de7"
+      >
+        <hm-uview-bg-card
+          width="100%"
+          height="100%"
+          border-radius=""
+          box-shadow-v-shadow=""
+          box-shadow-blur=""
+          class="ele-ff006dbd-abf0-427a-9076-d34d31098de7"
+        >
+          <view
+            class="ele-wrapper ele-wrapper-e4e2b49b-b097-4a65-a356-d9d95daa618b"
+          >
+            <hm-uview-text text="资料填写" :text-style="{ fontWeight: 700 }">
+            </hm-uview-text>
+          </view>
+          <view
+            class="ele-wrapper ele-wrapper-7927eb64-d6cf-4dda-8938-a15bb62187d2"
+          >
+            <hm-uview-bg-card
+              width="100%"
+              height="100%"
+              border-radius=""
+              box-shadow-v-shadow=""
+              box-shadow-blur=""
+              box-shadow-spread=""
+              class="ele-7927eb64-d6cf-4dda-8938-a15bb62187d2"
+            >
+              <view
+                class="ele-wrapper ele-wrapper-8b981cf2-ae14-4b8c-9f72-4cd6c41423a6"
+              >
+                <information-input-box
+                  :left-src="[
+                    '&quot;https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/touxiang.png&quot;',
+                    '&quot;https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/touxiang1.png&quot;',
+                  ]"
+                  value=""
+                >
+                </information-input-box>
+              </view>
+            </hm-uview-bg-card>
+          </view>
+        </hm-uview-bg-card>
+      </view>
     </view>
   </view>
 </template>
 
 <script>
-import { h } from 'vue';
+import { h } from "vue";
 import HmUviewBgCard from "/@/components/built-in/uniapp-uview-vue3/HmUviewBgCard.vue";
 import HmUviewText from "/@/components/built-in/uniapp-uview-vue3/HmUviewText.vue";
 import InformationInputBox from "/@/components/dkn-h-5/information-input-box/index.vue";
-
 
 export default {
   name: "RegistrationForm",
@@ -41,34 +67,29 @@ export default {
   data() {
     let self = this;
     return {
-  "8b981cf2-ae14-4b8c-9f72-4cd6c41423a6": {
-    "leftSrc": [
-      "\"https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/touxiang.png\"",
-      "\"https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/touxiang1.png\""
-    ],
-    "value": "1212"
-  }
-};
+      "8b981cf2-ae14-4b8c-9f72-4cd6c41423a6": {
+        leftSrc: [
+          '"https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/touxiang.png"',
+          '"https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/touxiang1.png"',
+        ],
+        value: "",
+      },
+    };
   },
-  watch: {
-  },
-  methods:{
-
-  }
+  watch: {},
+  methods: {},
 };
 </script>
 
 <style lang="less" scoped>
 .page {
-  
 }
 
 .ele-wrapper-ff006dbd-abf0-427a-9076-d34d31098de7 {
-  width:100%;
+  width: 100%;
 }
 
 .ele-wrapper-7927eb64-d6cf-4dda-8938-a15bb62187d2 {
-  width:100%;
+  width: 100%;
 }
-
 </style>
