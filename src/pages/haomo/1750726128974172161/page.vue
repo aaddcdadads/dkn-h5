fd783267d1b0a739d5dda5afd7b7954b95196d1b
胡小根 <hxg@haomo.com>
2024-01-26 11:46:50 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

--- src/pages/haomo/1750726128974172161/page.vue
+++ src/pages/haomo/1750726128974172161/page.vue
@@ -15,7 +15,23 @@
           <view
             class="ele-wrapper ele-wrapper-7e13b692-9cc2-4862-bad0-cf3303afbbaa"
           >
-            <hm-uview-bg-card> </hm-uview-bg-card>
+            <hm-uview-bg-card>
+              <view
+                class="ele-wrapper ele-wrapper-d5bbc53a-e10d-447f-ad1c-d820e1888444"
+              >
+                <hm-uview-text text="¥499.00" color="#F56C6C"> </hm-uview-text>
+              </view>
+              <view
+                class="ele-wrapper ele-wrapper-25945a72-1ec0-4b4a-8f27-c9f47a3be2a6"
+              >
+                <hm-uview-text
+                  text=" 剩下时间 23:59:59"
+                  font-size="12px"
+                  color="#999999"
+                >
+                </hm-uview-text>
+              </view>
+            </hm-uview-bg-card>
           </view>
         </hm-uview-bg-card>
       </view>
@@ -26,11 +42,13 @@
 <script>
 import { h } from "vue";
 import HmUviewBgCard from "/@/components/built-in/uniapp-uview-vue3/HmUviewBgCard.vue";
+import HmUviewText from "/@/components/built-in/uniapp-uview-vue3/HmUviewText.vue";
 
 export default {
   name: "PaymentOrder",
   components: {
     HmUviewBgCard,
+    HmUviewText,
   },
   options: { styleIsolation: "shared" },
   data() {
