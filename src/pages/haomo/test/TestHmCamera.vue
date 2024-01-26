2d68cc54e380f1a9b4b08424e1c573a88bf27605
胡小根 <hxg@haomo.com>
2024-01-26 17:51:17 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

---
+++ src/pages/haomo/test/TestHmCamera.vue
@@ -0,0 +1,27 @@
+<template>
+  <camera
+    device-position="back"
+    flash="auto"
+    @error="error"
+    style="width: 100%; height: 300px"
+  ></camera>
+</template>
+
+<script>
+import HmCamera from "@/components/built-in/uniapp/HmCamera.vue";
+
+export default {
+  components: { HmCamera },
+  data() {
+    return {};
+  },
+  methods: {
+    error(e) {
+      this.$emit(e.detail);
+    },
+  },
+};
+</script>
+
+<style lang="less">
+</style>
---
+++ src/pages/haomo/test/TestHmFollowCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <view>
+    <hm-follow-card></hm-follow-card>
+    <hm-follow-card
+      ref="TestHmFollowCard"
+      :options="TestHmFollowCard.options"
+    ></hm-follow-card>
+  </view>
+</template>
+
+<script>
+import HmFollowCard from "/@/components/built-in/uniapp-haomo/HmFollowCard.vue";
+export default {
+  components: { HmFollowCard },
+  data() {
+    return {
+      TestHmFollowCard: {
+        options: {
+          backgroudImage:
+            "https://block-design.oss-cn-shenzhen.aliyuncs.com/project-imgs/NUSEP/banner.png",
+          followText: "+ 关注",
+          name: "关注的人",
+          personImage:
+            "https://block-design.oss-cn-shenzhen.aliyuncs.com/project-imgs/NUSEP/%E5%A4%B4%E5%83%8F%402x.png",
+          fanNumbers: "784",
+          fanNumbersCompany: "个",
+          likeImage: "",
+          likeNumbers: "189",
+          likeNumbersCompany: "个",
+        },
+      },
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style lang="less" scoped></style>
