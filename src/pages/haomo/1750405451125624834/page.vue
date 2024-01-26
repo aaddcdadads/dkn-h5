2d8a4f1f5a6c2594bcb1935c557dabe25bb0aeed
胡小根 <hxg@haomo.com>
2024-01-26 18:00:48 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

--- src/pages/haomo/1750405451125624834/page.vue
+++ src/pages/haomo/1750405451125624834/page.vue
@@ -2,24 +2,15 @@
   <view class="page card">
     <view class="page-wrapper">
       <view
-        class="ele-wrapper ele-wrapper-3fc8887d-ebb4-48bd-b3e1-98e34169d622"
+        class="ele-wrapper ele-wrapper-6b016e0e-a5f1-4300-b1e0-0c13b00f4094"
       >
-        <hm-uview-bg-card> </hm-uview-bg-card>
+        <event-registration-card> </event-registration-card>
       </view>
       <view
-        class="ele-wrapper ele-wrapper-b6c2ba52-c586-491a-816b-a3168775e654"
+        class="ele-wrapper ele-wrapper-3db7938d-bfde-4dab-8407-1f4c36196318"
       >
-        <hm-uview-bg-card> </hm-uview-bg-card>
-      </view>
-      <view
-        class="ele-wrapper ele-wrapper-b9d9dfe6-0b8c-4c3c-9c96-0495d6256c51"
-      >
-        <hm-uview-bg-card> </hm-uview-bg-card>
-      </view>
-      <view
-        class="ele-wrapper ele-wrapper-fa443681-89f9-402f-b360-72d5f8de4c72"
-      >
-        <prize-list-component> </prize-list-component>
+        <prize-list-component class="ele-3db7938d-bfde-4dab-8407-1f4c36196318">
+        </prize-list-component>
       </view>
     </view>
   </view>
@@ -27,13 +18,13 @@
 
 <script>
 import { h } from "vue";
-import HmUviewBgCard from "/@/components/built-in/uniapp-uview-vue3/HmUviewBgCard.vue";
+import EventRegistrationCard from "/@/components/dkn-h-5/event-registration-card/index.vue";
 import PrizeListComponent from "/@/components/dkn-h-5/prize-list-component/index.vue";
 
 export default {
   name: "Test",
   components: {
-    HmUviewBgCard,
+    EventRegistrationCard,
     PrizeListComponent,
   },
   options: { styleIsolation: "shared" },
@@ -48,5 +39,9 @@ export default {
 
 <style lang="less" scoped>
 .page {
+}
+
+.ele-wrapper-3db7938d-bfde-4dab-8407-1f4c36196318 {
+  position: relative;
 }
 </style>
