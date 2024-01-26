c2e500c05f125bf95a40759ef83e4c54db78843a
胡小根 <hxg@haomo.com>
2024-01-26 11:55:27 +0800

    feat: NUSEP网站-更新设计云页面清单

--- src/pages/haomo/1750726128974172161/page.vue
+++ src/pages/haomo/1750726128974172161/page.vue
@@ -1,57 +1,34 @@
 <template>
   <view class="page card">
     <view class="page-wrapper">
-      <view
-        class="ele-wrapper ele-wrapper-cee9d9cc-cb1e-4b02-9600-85fa5cd8957a"
-      >
-        <hm-uview-bg-card
-          width="100%"
-          height="100%"
-          border-radius=""
-          box-shadow-v-shadow=""
-          box-shadow-blur=""
-          class="ele-cee9d9cc-cb1e-4b02-9600-85fa5cd8957a"
-        >
-          <view class="ele-wrapper ele-wrapper-titlePrice">
-            <hm-uview-bg-card
-              ref="titlePrice"
-              width="100%"
-              height="100%"
-              border-radius=""
-              box-shadow-v-shadow=""
-              box-shadow-blur=""
-              class="ele-titlePrice"
-            >
-              <view
-                class="ele-wrapper ele-wrapper-d5bbc53a-e10d-447f-ad1c-d820e1888444"
-              >
-                <hm-uview-text text="¥499.00" color="#F56C6C"> </hm-uview-text>
-              </view>
-              <view
-                class="ele-wrapper ele-wrapper-9e72204c-af42-497a-9f74-447633df49ca"
-              >
-                <hm-uview-icon
-                  name="clock"
-                  color="#999999"
-                  size="15px"
-                  label="剩下时间 23:59:59"
-                  label-color="#999999"
-                >
-                </hm-uview-icon>
-              </view>
-            </hm-uview-bg-card>
-          </view>
-        </hm-uview-bg-card>
-      </view>
+        <view class="ele-wrapper ele-wrapper-cee9d9cc-cb1e-4b02-9600-85fa5cd8957a">
+      <hm-uview-bg-card width="100%" height="100%" border-radius="" box-shadow-v-shadow="" box-shadow-blur="" class="ele-cee9d9cc-cb1e-4b02-9600-85fa5cd8957a"> 
+            <view class="ele-wrapper ele-wrapper-titlePrice">
+      <hm-uview-bg-card ref="titlePrice" width="100%" height="100%" border-radius="" box-shadow-v-shadow="" box-shadow-blur="" class="ele-titlePrice"> 
+            <view class="ele-wrapper ele-wrapper-d5bbc53a-e10d-447f-ad1c-d820e1888444">
+      <hm-uview-text text="¥499.00" color="#F56C6C"> 
+       
+    </hm-uview-text>
+    </view> 
+            <view class="ele-wrapper ele-wrapper-9e72204c-af42-497a-9f74-447633df49ca">
+      <hm-uview-icon name="clock" color="#999999" size="12px" label="剩下时间 23:59:59" label-size="12px" label-color="#999999"> 
+       
+    </hm-uview-icon>
+    </view> 
+    </hm-uview-bg-card>
+    </view> 
+    </hm-uview-bg-card>
+    </view>
     </view>
   </view>
 </template>
 
 <script>
-import { h } from "vue";
+import { h } from 'vue';
 import HmUviewBgCard from "/@/components/built-in/uniapp-uview-vue3/HmUviewBgCard.vue";
 import HmUviewText from "/@/components/built-in/uniapp-uview-vue3/HmUviewText.vue";
 import HmUviewIcon from "/@/components/built-in/uniapp-uview-vue3/HmUviewIcon.vue";
+
 
 export default {
   name: "PaymentOrder",
@@ -65,26 +42,31 @@ export default {
     let self = this;
     return {};
   },
-  watch: {},
-  methods: {},
+  watch: {
+  },
+  methods:{
+
+  }
 };
 </script>
 
 <style lang="less" scoped>
 .page {
+  
 }
 
 .ele-wrapper-cee9d9cc-cb1e-4b02-9600-85fa5cd8957a {
-  width: 100%;
-  height: 100vh;
+  width:100%;
+height:100vh;
 }
 
 .ele-wrapper-titlePrice {
-  width: 100%;
-  .ele-titlePrice {
-    display: flex;
+  width:100%;
+.ele-titlePrice{
+display: flex;
     flex-direction: column;
     align-items: center;
-  }
 }
+}
+
 </style>
