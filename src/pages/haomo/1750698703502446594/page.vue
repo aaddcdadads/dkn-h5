0522f23064c9845c8328cc1f4e284a2f75be8c7a
胡小根 <hxg@haomo.com>
2024-01-26 09:55:43 +0800

    feat: NUSEP网站-更新设计云页面清单

--- src/pages/haomo/1750698703502446594/page.vue
+++ src/pages/haomo/1750698703502446594/page.vue
@@ -1,15 +1,29 @@
 <template>
   <view class="page card">
-    <view class="page-wrapper"> </view>
+    <view class="page-wrapper">
+      <view
+        class="ele-wrapper ele-wrapper-df164ee6-d788-4ffb-81ec-3b7b59c75a7b"
+      >
+        <hm-uview-bg-card
+          width="100%"
+          height="100%"
+          class="ele-df164ee6-d788-4ffb-81ec-3b7b59c75a7b"
+        >
+        </hm-uview-bg-card>
+      </view>
+    </view>
   </view>
 </template>
 
 <script>
 import { h } from "vue";
+import HmUviewBgCard from "/@/components/built-in/uniapp-uview-vue3/HmUviewBgCard.vue";
 
 export default {
   name: "VerificationConfirmation",
-  components: {},
+  components: {
+    HmUviewBgCard,
+  },
   options: { styleIsolation: "shared" },
   data() {
     let self = this;
@@ -22,5 +36,9 @@ export default {
 
 <style lang="less" scoped>
 .page {
+}
+
+.ele-wrapper-df164ee6-d788-4ffb-81ec-3b7b59c75a7b {
+  width: 100%;
 }
 </style>
