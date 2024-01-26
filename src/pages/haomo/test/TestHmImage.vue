4aa888cc5bb3c96928e2b0331cd2a7ac507947cd
胡小根 <hxg@haomo.com>
2024-01-26 11:29:55 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

---
+++ src/pages/haomo/test/TestHmImage.vue
@@ -0,0 +1,43 @@
+<template>
+  <image
+    class="img"
+    src="https://img.yzcdn.cn/vant/cat.jpeg"
+    mode="scaleToFill"
+    @error="error"
+    @load="load"
+  >
+  </image>
+</template>
+
+<script>
+import HmImage from "@/components/built-in/uniapp/HmImage.vue";
+
+export default {
+  components: { HmImage },
+  data() {
+    return {};
+  },
+  methods: {
+    /**
+     * 加载完触发
+     */
+    load() {
+      console.log("图片加载");
+    },
+
+    /**
+     * 加载失败触发
+     */
+    error() {
+      console.log("图片加载失败");
+    },
+  },
+};
+</script>
+
+<style lang="less">
+.img {
+  width: 300px;
+  height: 200px;
+}
+</style>
---
+++ src/pages/haomo/test/TestHmSwipeList.vue
@@ -0,0 +1,42 @@
+<template>
+  <view>
+    <hm-swipe-list ref="default"></hm-swipe-list>
+    <hm-swipe-list
+      ref="HmSwipeList"
+      :list="HmSwipeList.list"
+      :url="HmSwipeList.url"
+      :params="HmSwipeList.params"
+      :status="HmSwipeList.status"
+      :contentText="HmSwipeList.contentText"
+      @clickLoadMore="onHmSwipeListClickLoadMore"
+    ></hm-swipe-list>
+  </view>
+</template>
+<script>
+import HmSwipeList from "@/components/built-in/uniapp-haomo/HmSwipeList.vue";
+export default {
+  components: { HmSwipeList },
+  data() {
+    return {
+      HmSwipeList: {
+        list: [],
+        url: "",
+        params: {},
+        status: "more",
+        contentText: {
+          show: true,
+          contentdown: "-显示更多-",
+          contentrefresh: "正在加载...",
+          contentnomore: "没有更多数据了",
+        },
+      },
+    };
+  },
+  mounted() {},
+  methods: {
+    onHmSwipeListClickLoadMore() {
+        this.HmSwipeList.status = "loading"
+    },
+  },
+};
+</script>
