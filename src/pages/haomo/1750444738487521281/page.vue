acddfeb077ce2400833e8c42e0639513bb58c0bb
胡小根 <hxg@haomo.com>
2024-01-26 11:06:34 +0800

    feat: NUSEP网站-更新设计云页面清单

--- src/pages/haomo/1750444738487521281/page.vue
+++ src/pages/haomo/1750444738487521281/page.vue
@@ -118,6 +118,7 @@
                 <hm-uview-button
                   :type="'primary'"
                   :shape="'circle'"
+                  text="立即报名"
                   class="ele-acf99afa-7e7b-42dc-a52d-aa67ffc5af5b"
                 >
                 </hm-uview-button>
