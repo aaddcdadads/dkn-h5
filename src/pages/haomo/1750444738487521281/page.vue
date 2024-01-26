7874fad8744a268a643a5d2bc6b225649692e06e
胡小根 <hxg@haomo.com>
2024-01-26 11:05:41 +0800

    feat: NUSEP网站-更新设计云页面清单

--- src/pages/haomo/1750444738487521281/page.vue
+++ src/pages/haomo/1750444738487521281/page.vue
@@ -1,136 +1,69 @@
 <template>
   <view class="page card">
     <view class="page-wrapper">
-      <view class="ele-wrapper ele-wrapper-formbgCard">
-        <hm-uview-bg-card
-          ref="formbgCard"
-          width="100%"
-          height="100%"
-          border-radius=""
-          box-shadow-v-shadow=""
-          box-shadow-blur=""
-          background-color="#F2F2F2"
-          class="ele-formbgCard"
-        >
-          <view
-            class="ele-wrapper ele-wrapper-e4e2b49b-b097-4a65-a356-d9d95daa618b"
-          >
-            <hm-uview-text text="资料填写" :text-style="{ fontWeight: 700 }">
-            </hm-uview-text>
-          </view>
-          <view
-            class="ele-wrapper ele-wrapper-7927eb64-d6cf-4dda-8938-a15bb62187d2"
-          >
-            <hm-uview-bg-card
-              width="100%"
-              height="100%"
-              border-radius=""
-              box-shadow-v-shadow=""
-              box-shadow-blur=""
-              box-shadow-spread=""
-              background-color="#F2F2F2"
-              class="ele-7927eb64-d6cf-4dda-8938-a15bb62187d2"
-            >
-              <view
-                class="ele-wrapper ele-wrapper-8b981cf2-ae14-4b8c-9f72-4cd6c41423a6"
-              >
-                <information-input-box
-                  value=""
-                  placeholder="* 请填写姓名/昵称"
-                  class="ele-8b981cf2-ae14-4b8c-9f72-4cd6c41423a6"
-                >
-                </information-input-box>
-              </view>
-              <view
-                class="ele-wrapper ele-wrapper-9662da08-d680-4c34-af33-b2ba9794437a"
-              >
-                <information-input-box
-                  :left-src="[
-                    'https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/shoujis.png',
-                    'https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/shouji.png',
-                  ]"
-                  value=""
-                  placeholder="* 请填写手机号码"
-                  class="ele-9662da08-d680-4c34-af33-b2ba9794437a"
-                >
-                </information-input-box>
-              </view>
-              <view
-                class="ele-wrapper ele-wrapper-5102357a-cd29-4bb3-9a1f-ab709e15c3a8"
-              >
-                <information-input-box
-                  :left-src="[
-                    'https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/xinxis.png',
-                    'https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/xinxi.png',
-                  ]"
-                  value=""
-                  :show-code="true"
-                  placeholder="* 请填写短信验证码"
-                  class="ele-5102357a-cd29-4bb3-9a1f-ab709e15c3a8"
-                >
-                </information-input-box>
-              </view>
-              <view
-                class="ele-wrapper ele-wrapper-71e123e5-88a9-44c5-b029-f89fdaffa0d3"
-              >
-                <information-input-box
-                  :left-src="[
-                    'https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/mendian.png',
-                    'https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/mendians.png',
-                  ]"
-                  value=""
-                  :show-icon="true"
-                  placeholder="* 请选择领奖门店"
-                  class="ele-71e123e5-88a9-44c5-b029-f89fdaffa0d3"
-                >
-                </information-input-box>
-              </view>
-            </hm-uview-bg-card>
-          </view>
-          <view
-            class="ele-wrapper ele-wrapper-12afd4d4-40cb-4c92-9871-dbe45f394ddf"
-          >
-            <hm-uview-bg-card
-              width="100%"
-              height="100%"
-              border-radius=""
-              padding=""
-              box-shadow-v-shadow=""
-              box-shadow-blur=""
-              background-color="#F2F2F2"
-              class="ele-12afd4d4-40cb-4c92-9871-dbe45f394ddf"
-            >
-              <view
-                class="ele-wrapper ele-wrapper-4691aa84-ddc5-46bf-bd31-d46908cb517f"
-              >
-                <hm-uview-text text="报名费用" font-size="14px" color="#409EFF">
-                </hm-uview-text>
-              </view>
-              <view
-                class="ele-wrapper ele-wrapper-c5aebda0-b42d-4d23-a10d-b223a5145510"
-              >
-                <hm-uview-text text="¥29.00" font-size="14px" color="#409EFF">
-                </hm-uview-text>
-              </view>
-              <view
-                class="ele-wrapper ele-wrapper-acf99afa-7e7b-42dc-a52d-aa67ffc5af5b"
-              >
-                <hm-uview-button :type="'primary'"> </hm-uview-button>
-              </view>
-            </hm-uview-bg-card>
-          </view>
-        </hm-uview-bg-card>
-      </view>
+        <view class="ele-wrapper ele-wrapper-formbgCard">
+      <hm-uview-bg-card ref="formbgCard" width="100%" height="100%" border-radius="" box-shadow-v-shadow="" box-shadow-blur="" background-color="#F2F2F2" class="ele-formbgCard"> 
+            <view class="ele-wrapper ele-wrapper-e4e2b49b-b097-4a65-a356-d9d95daa618b">
+      <hm-uview-text text="资料填写" :text-style='{"fontWeight":700}'> 
+       
+    </hm-uview-text>
+    </view> 
+            <view class="ele-wrapper ele-wrapper-7927eb64-d6cf-4dda-8938-a15bb62187d2">
+      <hm-uview-bg-card width="100%" height="100%" border-radius="" box-shadow-v-shadow="" box-shadow-blur="" box-shadow-spread="" background-color="#F2F2F2" class="ele-7927eb64-d6cf-4dda-8938-a15bb62187d2"> 
+            <view class="ele-wrapper ele-wrapper-8b981cf2-ae14-4b8c-9f72-4cd6c41423a6">
+      <information-input-box value="" placeholder="* 请填写姓名/昵称" class="ele-8b981cf2-ae14-4b8c-9f72-4cd6c41423a6"> 
+       
+    </information-input-box>
+    </view> 
+            <view class="ele-wrapper ele-wrapper-9662da08-d680-4c34-af33-b2ba9794437a">
+      <information-input-box :left-src='["https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/shoujis.png","https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/shouji.png"]' value="" placeholder="* 请填写手机号码" class="ele-9662da08-d680-4c34-af33-b2ba9794437a"> 
+       
+    </information-input-box>
+    </view> 
+            <view class="ele-wrapper ele-wrapper-5102357a-cd29-4bb3-9a1f-ab709e15c3a8">
+      <information-input-box :left-src='["https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/xinxis.png","https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/xinxi.png"]' value="" :show-code="true" placeholder="* 请填写短信验证码" class="ele-5102357a-cd29-4bb3-9a1f-ab709e15c3a8"> 
+       
+    </information-input-box>
+    </view> 
+            <view class="ele-wrapper ele-wrapper-71e123e5-88a9-44c5-b029-f89fdaffa0d3">
+      <information-input-box :left-src='["https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/mendian.png","https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/mendians.png"]' value="" :show-icon="true" placeholder="* 请选择领奖门店" class="ele-71e123e5-88a9-44c5-b029-f89fdaffa0d3"> 
+       
+    </information-input-box>
+    </view> 
+    </hm-uview-bg-card>
+    </view> 
+            <view class="ele-wrapper ele-wrapper-12afd4d4-40cb-4c92-9871-dbe45f394ddf">
+      <hm-uview-bg-card width="100%" height="100%" border-radius="" padding="" box-shadow-v-shadow="" box-shadow-blur="" background-color="#F2F2F2" class="ele-12afd4d4-40cb-4c92-9871-dbe45f394ddf"> 
+            <view class="ele-wrapper ele-wrapper-4691aa84-ddc5-46bf-bd31-d46908cb517f">
+      <hm-uview-text text="报名费用" font-size="14px" color="#409EFF"> 
+       
+    </hm-uview-text>
+    </view> 
+            <view class="ele-wrapper ele-wrapper-c5aebda0-b42d-4d23-a10d-b223a5145510">
+      <hm-uview-text text="¥29.00" font-size="14px" color="#409EFF"> 
+       
+    </hm-uview-text>
+    </view> 
+            <view class="ele-wrapper ele-wrapper-acf99afa-7e7b-42dc-a52d-aa67ffc5af5b">
+      <hm-uview-button :type="'primary'" class="ele-acf99afa-7e7b-42dc-a52d-aa67ffc5af5b"> 
+       
+    </hm-uview-button>
+    </view> 
+    </hm-uview-bg-card>
+    </view> 
+    </hm-uview-bg-card>
+    </view>
     </view>
   </view>
 </template>
 
 <script>
-import { h } from "vue";
+import { h } from 'vue';
 import HmUviewBgCard from "/@/components/built-in/uniapp-uview-vue3/HmUviewBgCard.vue";
 import HmUviewText from "/@/components/built-in/uniapp-uview-vue3/HmUviewText.vue";
 import InformationInputBox from "/@/components/dkn-h-5/information-input-box/index.vue";
 import HmUviewButton from "/@/components/built-in/uniapp-uview-vue3/HmUviewButton.vue";
+
 
 export default {
   name: "RegistrationForm",
@@ -144,76 +77,85 @@ export default {
   data() {
     let self = this;
     return {
-      "8b981cf2-ae14-4b8c-9f72-4cd6c41423a6": {
-        value: "",
-      },
-      "9662da08-d680-4c34-af33-b2ba9794437a": {
-        leftSrc: [
-          "https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/shoujis.png",
-          "https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/shouji.png",
-        ],
-        value: "",
-      },
-      "5102357a-cd29-4bb3-9a1f-ab709e15c3a8": {
-        leftSrc: [
-          "https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/xinxis.png",
-          "https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/xinxi.png",
-        ],
-        value: "",
-      },
-      "71e123e5-88a9-44c5-b029-f89fdaffa0d3": {
-        leftSrc: [
-          "https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/mendian.png",
-          "https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/mendians.png",
-        ],
-        value: "",
-      },
-    };
+  "8b981cf2-ae14-4b8c-9f72-4cd6c41423a6": {
+    "value": ""
   },
-  watch: {},
-  methods: {},
+  "9662da08-d680-4c34-af33-b2ba9794437a": {
+    "leftSrc": [
+      "https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/shoujis.png",
+      "https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/shouji.png"
+    ],
+    "value": ""
+  },
+  "5102357a-cd29-4bb3-9a1f-ab709e15c3a8": {
+    "leftSrc": [
+      "https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/xinxis.png",
+      "https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/xinxi.png"
+    ],
+    "value": ""
+  },
+  "71e123e5-88a9-44c5-b029-f89fdaffa0d3": {
+    "leftSrc": [
+      "https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/mendian.png",
+      "https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/mendians.png"
+    ],
+    "value": ""
+  }
+};
+  },
+  watch: {
+  },
+  methods:{
+
+  }
 };
 </script>
 
 <style lang="less" scoped>
 .page {
+  
 }
 
 .ele-wrapper-formbgCard {
-  width: 100%;
-  height: 100vh;
-  .ele-formbgCard {
+  width:100%;
+height:100vh;
+.ele-formbgCard{
     display: flex;
     flex-direction: column;
     align-items: center;
-  }
+}
 }
 
 .ele-wrapper-7927eb64-d6cf-4dda-8938-a15bb62187d2 {
-  width: 100%;
+  width:100%;
 }
 
 .ele-wrapper-8b981cf2-ae14-4b8c-9f72-4cd6c41423a6 {
-  width: 100%;
-  margin-top: 20px;
+  width:100%;
+margin-top:20px;
 }
 
 .ele-wrapper-9662da08-d680-4c34-af33-b2ba9794437a {
-  width: 100%;
-  margin-top: 20px;
+  width:100%;
+margin-top:20px;
 }
 
 .ele-wrapper-5102357a-cd29-4bb3-9a1f-ab709e15c3a8 {
-  width: 100%;
-  margin-top: 20px;
+  width:100%;
+margin-top:20px;
 }
 
 .ele-wrapper-71e123e5-88a9-44c5-b029-f89fdaffa0d3 {
-  width: 100%;
-  margin-top: 20px;
+  width:100%;
+margin-top:20px;
 }
 
 .ele-wrapper-12afd4d4-40cb-4c92-9871-dbe45f394ddf {
-  width: 100%;
+  width:100%;
 }
+
+.ele-wrapper-acf99afa-7e7b-42dc-a52d-aa67ffc5af5b {
+  width:100%;
+}
+
 </style>
