1ba513c58806d6323f72cdf5de92df342a1eeef4
胡小根 <hxg@haomo.com>
2024-01-27 17:55:59 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

--- src/pages/haomo/1750444738487521281/page.vue
+++ src/pages/haomo/1750444738487521281/page.vue
@@ -122,6 +122,7 @@
                 <information-input-box
                   :left-src="[
                     'https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/xinxi_1.png',
+                    'https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/xinxi.png',
                   ]"
                   value=""
                   :show-code="true"
@@ -473,6 +474,7 @@ export default {
       "5102357a-cd29-4bb3-9a1f-ab709e15c3a8": {
         leftSrc: [
           "https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/xinxi_1.png",
+          "https://hm-static-img.oss-cn-beijing.aliyuncs.com/DecathlonSpringFestivalActivities/xinxi.png",
         ],
         value: "",
       },
