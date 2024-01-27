d57e31409e2a5b9f71d9f763f8482fa70bff8c5f
胡小根 <hxg@haomo.com>
2024-01-27 17:46:10 +0800

    feat: NUSEP网站-更新设计云页面清单

--- src/pages/haomo/1750444738487521281/page.vue
+++ src/pages/haomo/1750444738487521281/page.vue
@@ -170,6 +170,83 @@
                 class="ele-wrapper ele-wrapper-38ef40fd-ce54-482c-83b9-ef433f2f2aa3"
               >
                 <store-selection
+                  :list="[
+                    {
+                      city: '常德',
+                      shop: [
+                        {
+                          shopname: '柳叶店',
+                          address:
+                            '常德市武陵区柳叶大道与皂果交汇处万达广场一楼',
+                          checked: false,
+                        },
+                      ],
+                    },
+                    {
+                      city: '长沙',
+                      shop: [
+                        {
+                          shopname: '梅溪湖店',
+                          address: '湖南省长沙市丘林分三路1099号',
+                          checked: false,
+                        },
+                        {
+                          shopname: '湘江店路',
+                          address: '湖南省长沙市丘林分三路1099号',
+                          checked: false,
+                        },
+                        {
+                          shopname: '洋湖店',
+                          address: '湖南省长沙市丘林分三路1099号',
+                          checked: false,
+                        },
+                        {
+                          shopname: '洋湖店',
+                          address: '湖南省长沙市丘林分三路1099号',
+                          checked: false,
+                        },
+                      ],
+                    },
+                    {
+                      city: '成都',
+                      shop: [
+                        {
+                          shopname: '梅溪湖店',
+                          address: '湖南省长沙市丘林分三路1099号',
+                          checked: false,
+                        },
+                        {
+                          shopname: '湘江店路',
+                          address:
+                            '成都市郫都区德源镇红旗大道北段221号5号楼11楼',
+                          checked: false,
+                        },
+                        {
+                          shopname: '洋湖店',
+                          address: '成都高新区仁和街39号6栋2层1号',
+                          checked: false,
+                        },
+                        {
+                          shopname: '洋湖店',
+                          address:
+                            '中国（四川）自由贸易试验区成都高新区益州大道中段1800号1栋22-23层2201号、2301号',
+                          checked: false,
+                        },
+                        {
+                          shopname: '洋湖店',
+                          address:
+                            '中国（四川）自由贸易试验区成都市天府新区万安街道麓山大道二段18号附3号4栋-1层2号',
+                          checked: false,
+                        },
+                        {
+                          shopname: '洋湖店',
+                          address:
+                            '中国（四川）自由贸易试验区成都高新区吉瑞四路399号1栋8楼5、6号',
+                          checked: false,
+                        },
+                      ],
+                    },
+                  ]"
                   class="ele-38ef40fd-ce54-482c-83b9-ef433f2f2aa3"
                 >
                 </store-selection>
@@ -615,7 +692,7 @@ export default {
   font-size: 16px;
   color: #333333;
   line-height: 20px;
-  margin: 10px 0;
+  margin: 20px 0;
 }
 
 .ele-wrapper-38ef40fd-ce54-482c-83b9-ef433f2f2aa3 {
