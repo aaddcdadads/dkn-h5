6970ee36396fb55457c1f9b6ec059d6301097f23
胡小根 <hxg@haomo.com>
2024-01-27 11:50:18 +0800

    feat: NUSEP网站-更新设计云页面清单

--- src/pages/haomo/1751068398554451969/page.vue
+++ src/pages/haomo/1751068398554451969/page.vue
@@ -184,7 +184,7 @@
                         <hm-uview-field
                           value=""
                           label-width=""
-                          maxlength="100%"
+                          maxlength=""
                           icon=""
                           right-icon=""
                           class="ele-929420c8-3de3-499f-87e8-c054a0fa0583"
