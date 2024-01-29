4d6f38baab347b40de4b3544372483030f5bc4bf
胡小根 <hxg@haomo.com>
2024-01-29 17:28:06 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

--- src/pages/haomo/1751899680905629697/page.vue
+++ src/pages/haomo/1751899680905629697/page.vue
@@ -1,19 +1,47 @@
 <template>
   <view class="page card">
-    <view class="page-wrapper"> </view>
+    <view class="page-wrapper">
+      <view
+        class="ele-wrapper ele-wrapper-dbd7f3bd-da08-446b-beec-b1dc37f4621c"
+      >
+        <hm-uview-bg-card
+          width="100%"
+          height=""
+          border-radius="0"
+          padding="0"
+          box-shadow-color="#00000000"
+          class="ele-dbd7f3bd-da08-446b-beec-b1dc37f4621c"
+        >
+          <view
+            class="ele-wrapper ele-wrapper-84232644-133f-463c-b6ce-1d727f86aff6"
+          >
+            <hm-uview-modal :visible="true"> </hm-uview-modal>
+          </view>
+        </hm-uview-bg-card>
+      </view>
+    </view>
   </view>
 </template>
 
 <script>
 import { h } from "vue";
+import HmUviewBgCard from "/@/components/built-in/uniapp-uview-vue3/HmUviewBgCard.vue";
+import HmUviewModal from "/@/components/built-in/uniapp-uview-vue3/HmUviewModal.vue";
 
 export default {
   name: "ActivetyView",
-  components: {},
+  components: {
+    HmUviewBgCard,
+    HmUviewModal,
+  },
   options: { styleIsolation: "shared" },
   data() {
     let self = this;
-    return {};
+    return {
+      "84232644-133f-463c-b6ce-1d727f86aff6": {
+        visible: true,
+      },
+    };
   },
   watch: {},
   methods: {},
@@ -22,5 +50,9 @@ export default {
 
 <style lang="less" scoped>
 .page {
+}
+
+.ele-wrapper-dbd7f3bd-da08-446b-beec-b1dc37f4621c {
+  width: 100%;
 }
 </style>
