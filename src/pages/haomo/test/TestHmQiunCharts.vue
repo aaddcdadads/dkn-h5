b359dd3adb696acabd55a9bae233523b2163097d
胡小根 <hxg@haomo.com>
2024-01-31 17:59:49 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

---
+++ src/pages/haomo/test/TestHmQiunCharts.vue
@@ -0,0 +1,16 @@
+<template>
+  <view>
+    <hm-qiun-charts></hm-qiun-charts>
+  </view>
+</template>
+
+<script>
+import HmQiunCharts from "@/components/built-in/qiun-data-charts/HmQiunCharts.vue";
+
+export default {
+  components: { HmQiunCharts },
+};
+</script>
+
+<style lang="less">
+</style>
---
+++ src/pages/haomo/test/TestHmUviewBadge.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- <hm-uview-button @click="click">123</hm-uview-button> -->
+  <view>
+    <hm-uview-badge></hm-uview-badge>
+    <hm-uview-badge
+      :isDot="false"
+      count="15"
+      :offset="arr"
+      type="success"
+      color="#b63131"
+      bgcolor="#121a2a"
+    ></hm-uview-badge>
+    <hm-uview-badge
+      :isDot="true"
+      :count="2"
+      :offset="arr"
+      type="primary"
+      color="#b62521"
+      bgcolor="##2e841a"
+    ></hm-uview-badge>
+  </view>
+</template>
+
+<script>
+import HmUviewBadge from "@/components/built-in/uniapp-uview-vue3/HmUviewBadge.vue";
+export default {
+  name: "TestHmUviewBadge",
+  components: { HmUviewBadge },
+  data() {
+    return {
+      arr: [-8, -8],
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style>
+</style>
