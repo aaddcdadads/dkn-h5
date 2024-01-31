80b906b03be4dba3fbcb123ac9b2c50673d5b4c7
胡小根 <hxg@haomo.com>
2024-01-31 19:06:33 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

---
+++ src/components/dkn-h-5/hm-h-5-scan-code/index.vue
@@ -0,0 +1,77 @@
+<template>
+    <mumu-get-qrcode 
+      :continue="continue"
+      :exact="exact"
+      :size="size"
+      :definition="definition"  
+      @success='qrcodeSucess' 
+      @error="qrcodeError" 
+    >  
+    </mumu-get-qrcode>
+</template>
+
+<script>
+import mumuGetQrcode from '@/uni_modules/mumu-getQrcode/components/mumu-getQrcode/mumu-getQrcode.vue'
+
+export default {
+  name: "HmQrcode",
+  components: {
+    mumuGetQrcode
+  },
+  props: {
+    /**
+     * 连续监听
+     */
+    continue: {
+			type: Boolean,
+			default: false 
+		},
+    /**
+     * 前后摄像头
+     * @type Enum
+     * @options ["environment","user"]
+     */
+		exact: {
+			type: String,
+			default: 'environment' 
+		},
+    /**
+     * 显示区域
+     * @type Enum
+     * @options ["whole","balf"]
+     */
+		size: {
+			type: String,
+			default: 'whole'
+		},
+    /**
+     * 高清
+     */
+		definition: {
+			type: Boolean,
+			default: false
+		}
+  },
+  data() {
+    return {
+    }
+  },
+  watch: {},
+
+  mounted() {
+  },
+  beforeDestroy(){
+  },
+  methods: {
+    qrcodeSucess(data) {
+      this.$emit("qrcodeSucess", data);
+    },
+    qrcodeError(err) {
+      this.$emit("qrcodeError", err);
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
