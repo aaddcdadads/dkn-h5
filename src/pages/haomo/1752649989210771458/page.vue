ce9c1dc5cdbb35ba207df5841afd52e8d1ac71d7
胡小根 <hxg@haomo.com>
2024-01-31 21:29:02 +0800

    feat: NUSEP网站-更新设计云页面清单

--- src/pages/haomo/1752649989210771458/page.vue
+++ src/pages/haomo/1752649989210771458/page.vue
@@ -1,12 +1,11 @@
 <template>
   <view class="page card">
     <view class="page-wrapper">
-      <view
-        class="ele-wrapper ele-wrapper-6712ce3b-b09f-496d-8e43-6c9b2467065e"
-      >
+      <view class="ele-wrapper ele-wrapper-hmH5ScanCode">
         <hm-h-5-scan-code
-          @qrcodeSucess="onEle6712Ce3BB09F496D8E436C9B2467065EQrcodeSucess"
-          @qrcodeError="onEle6712Ce3BB09F496D8E436C9B2467065EQrcodeError"
+          ref="hmH5ScanCode"
+          @qrcodeSucess="onHmH5ScanCodeQrcodeSucess"
+          @qrcodeError="onHmH5ScanCodeQrcodeError"
         >
         </hm-h-5-scan-code>
       </view>
@@ -30,55 +29,42 @@ export default {
   },
   watch: {},
   methods: {
-    onEle6712Ce3BB09F496D8E436C9B2467065EQrcodeSucess(data) {
+    onHmH5ScanCodeQrcodeSucess(data) {
       //如果选择签到的培训管理和扫描出来的不一致
-      if (data != this.cultivateId) {
+      if (!data) {
         uni.showToast({
           title: "请选择正确的二维码",
           icon: "none",
           duration: 2500,
         });
+        uni.navigateBack({
+          delta: 1,
+        });
       } else {
-        this.$getAction("/api/jikong/signIn/list", {
-          cultivateId: data,
-          sysUserId: this.userInfo.id,
+        this.$getAction("/api/dkn/store/queryById", {
+          id: data,
         }).then((res) => {
-          if (res.success) {
-            if (res.result.records.length > 0) {
-              uni.showToast({
-                title: "你已签到",
-                icon: "success",
-                duration: 2500,
-              });
-            } else {
-              this.$postAction("/api/jikong/signIn/add", {
-                cultivateId: data,
-                sysUserId: this.userInfo.id,
-              }).then((item) => {
-                if (item.success) {
-                  uni.showToast({
-                    title: "签到成功",
-                    icon: "success",
-                    duration: 2500,
-                  });
-                }
-              });
-            }
-          } else {
+          if (!res.success) {
             uni.showToast({
-              title: "请求失败",
-              icon: "error",
+              title: "请选择正确的二维码",
+              icon: "none",
               duration: 2500,
             });
+            uni.navigateBack({
+              delta: 1,
+            });
           }
+          uni.navigateTo({
+            url:
+              "/pages/haomo/1751068398554451969/page?orderId=" +
+              this.orderId +
+              "&storeId=" +
+              data,
+          });
         });
       }
-
-      uni.navigateBack({
-        delta: 1,
-      });
     },
-    onEle6712Ce3BB09F496D8E436C9B2467065EQrcodeError(err) {
+    onHmH5ScanCodeQrcodeError(err) {
       uni.showToast({
         title: "摄像头授权失败",
         icon: "error",
