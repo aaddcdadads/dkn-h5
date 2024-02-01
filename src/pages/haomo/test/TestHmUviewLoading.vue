c11e8a81367bde0868e65d4f9c5ec87a0576fe69
胡小根 <hxg@haomo.com>
2024-02-01 15:24:41 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

---
+++ src/pages/haomo/test/TestHmUviewLoading.vue
@@ -0,0 +1,16 @@
+<template>
+  <view>
+    <hm-uview-loading></hm-uview-loading>
+    <hm-uview-loading mode="flower" size="100" :show="true"></hm-uview-loading>
+  </view>
+</template>
+
+<script>
+import HmUviewLoading from "@/components/built-in/uniapp-uview-vue3/HmUviewLoading.vue";
+
+export default {
+  components: { HmUviewLoading },
+};
+</script>
+
+<style lang="less"></style>
