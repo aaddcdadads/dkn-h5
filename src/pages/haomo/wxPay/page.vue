0f0299c3d9392af3e1b46288f1f786cbaf9ef56e
huangzhuoran <huangzhuoran@haomo-studio.com>
2024-02-01 12:19:38 +0800

    fix: 修改支付

--- src/pages/haomo/wxPay/page.vue
+++ src/pages/haomo/wxPay/page.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>
-    <u-image
-      :src="src"
-      :width="100"
-      :height="100"
-    />
+  <div class="container">
+    <div style="width: 100%;display: flex;margin-top:50px;justify-content: center;">
+      <u-image
+        :src="src"
+        :width="300"
+        :height="300"
+      />
+    </div>
+    <div class="tip">
+        <div class="ico-scan"></div>
+        <div class="tip-text">
+            <p>打开微信扫一扫</p>
+            <p>手机使用相册扫</p>
+        </div>
+    </div>
   </div>
 </template>
 <script>
@@ -42,5 +51,31 @@ export default {
 }
 </script>
 <style scoped>
+.container {
+  display: flex;
+  justify-content: center; /* 水平居中 */
+  flex-wrap: wrap;
+}
+.tip {
+  margin-top:50px;
+  width: 100%;
+  display: flex;
+  justify-content: center; /* 水平居中 */
+}
 
+.tip .ico-scan {
+  width: 56px;
+  height: 55px;
+  background: url("/static/kk.png") 0 0 no-repeat;
+  background-size: 56px 55px;
+  vertical-align: middle;
+}
+
+.tip .tip-text{
+  vertical-align: middle;
+  text-align: left;
+  margin-left: 23px;
+  font-size: 16px;
+  line-height: 28px;
+}
 </style>
