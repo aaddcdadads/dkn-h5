eb30ca88dbbd84824473a940dfb0a06d58a1200c
胡小根 <hxg@haomo.com>
2024-02-02 18:42:08 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

---
+++ src/pages/haomo/test/TestHmRichText.vue
@@ -0,0 +1,43 @@
+<template>
+  <rich-text
+    :nodes="nodes"
+    space="&nbsp"
+    selectable="true"
+    @itemclick="itemclick"
+  ></rich-text>
+</template>
+
+<script>
+import HmRichText from "@/components/built-in/uniapp/HmRichText.vue";
+
+export default {
+  components: { HmRichText },
+  data() {
+    return {
+      nodes: [
+        {
+          name: "div",
+          attrs: {
+            class: "div-class",
+            style: "line-height: 60px; color: red; text-align:center;",
+          },
+          children: [
+            {
+              type: "text",
+              text: "Hello&nbsp;uni-app!",
+            },
+          ],
+        },
+      ],
+    };
+  },
+  methods: {
+    itemclick() {
+      this.$emit("itemclick");
+    },
+  },
+};
+</script>
+
+<style lang="less">
+</style>
---
+++ src/pages/haomo/test/TestHmText.vue
@@ -0,0 +1,26 @@
+<template>
+  <text
+    text="aa"
+    font-size="24px"
+    color="rgb(0,0,0,1)"
+    @click="onClick"
+    selectable="false"
+    userSelect="false"
+    decode="false"
+  >
+  </text>
+</template>
+
+<script>
+import HmText from "@/components/built-in/uniapp/HmText.vue";
+
+export default {
+  components: { HmText },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="less">
+</style>
