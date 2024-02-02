a3303a638147691524b1a1f1bbc8a02df57ac01d
胡小根 <hxg@haomo.com>
2024-02-02 18:41:57 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

--- src/pages/haomo/1750444738487521281/page.vue
+++ src/pages/haomo/1750444738487521281/page.vue
@@ -1072,7 +1072,7 @@ export default {
       self.activityId = self.$route.query.activityId;
       self.activityName = self.$route.query.activityName;
       self.eventCard.list = [];
-      self.storeList.list = [];
+      //self.storeList.list = []
       self.payButton.text = `立即报名`;
       self.money = 0;
       self.activityText.text = "";
@@ -1113,7 +1113,12 @@ export default {
             shop: x.store,
           };
         });
-        self.storeList.list = [{ anchor: "b", store }];
+        self.storeList.list = [
+          {
+            anchor: "b",
+            store,
+          },
+        ];
       };
       self.getImg = function (url) {
         if (url.substring(0, 4) === "http") {
