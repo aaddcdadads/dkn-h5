4bb5928030242ceca6e6cb7de9531d59fb94d83b
胡小根 <hxg@haomo.com>
2024-02-02 16:23:37 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

--- src/pages/haomo/1750444738487521281/page.vue
+++ src/pages/haomo/1750444738487521281/page.vue
@@ -91,7 +91,7 @@
               width="100%"
               height="100%"
               border-radius=""
-              padding="8"
+              padding="18"
               box-shadow-v-shadow=""
               box-shadow-blur=""
               box-shadow-spread=""
