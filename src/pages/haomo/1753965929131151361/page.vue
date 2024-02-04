3abcb906c418cd4e7325df232b0fea00b7c23ec7
胡小根 <hxg@haomo.com>
2024-02-04 10:18:44 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

--- src/pages/haomo/1753965929131151361/page.vue
+++ src/pages/haomo/1753965929131151361/page.vue
@@ -1,26 +1,44 @@
 <template>
   <view class="page card">
-    <view class="page-wrapper"> </view>
+    <view class="page-wrapper">
+        <view class="ele-wrapper ele-wrapper-aeb7bbd5-76d1-4b56-8fcf-4839bc19aa46">
+      <hm-uview-bg-card width="100%" height="100%" border-radius="" box-shadow-v-shadow="" box-shadow-blur="" class="ele-aeb7bbd5-76d1-4b56-8fcf-4839bc19aa46"> 
+    </hm-uview-bg-card>
+    </view>
+    </view>
   </view>
 </template>
 
 <script>
-import { h } from "vue";
+import { h } from 'vue';
+import HmUviewBgCard from "/@/components/built-in/uniapp-uview-vue3/HmUviewBgCard.vue";
+
 
 export default {
   name: "NewActivityRegistrationInformation",
-  components: {},
+  components: {
+    HmUviewBgCard,
+  },
   options: { styleIsolation: "shared" },
   data() {
     let self = this;
     return {};
   },
-  watch: {},
-  methods: {},
+  watch: {
+  },
+  methods:{
+
+  }
 };
 </script>
 
 <style lang="less" scoped>
 .page {
+  
 }
+
+.ele-wrapper-aeb7bbd5-76d1-4b56-8fcf-4839bc19aa46 {
+  width:100%;
+}
+
 </style>
