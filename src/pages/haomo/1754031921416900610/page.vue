5a5347d210d3ac09e8547246724b8679f1e19bcb
胡小根 <hxg@haomo.com>
2024-02-04 14:40:12 +0800

    feat: NUSEP网站-更新设计云页面清单

--- src/pages/haomo/1754031921416900610/page.vue
+++ src/pages/haomo/1754031921416900610/page.vue
@@ -1,15 +1,31 @@
 <template>
   <view class="page card">
-    <view class="page-wrapper"> </view>
+    <view class="page-wrapper">
+      <view
+        class="ele-wrapper ele-wrapper-d2f3fb59-dd6f-46a0-9ab7-5a867871dbd7"
+      >
+        <hm-uview-bg-card
+          width="100%"
+          height="100%"
+          border-radius=""
+          box-shadow-v-shadow=""
+          box-shadow-blur=""
+        >
+        </hm-uview-bg-card>
+      </view>
+    </view>
   </view>
 </template>
 
 <script>
 import { h } from "vue";
+import HmUviewBgCard from "/@/components/built-in/uniapp-uview-vue3/HmUviewBgCard.vue";
 
 export default {
   name: "NewVerificationConfirmation",
-  components: {},
+  components: {
+    HmUviewBgCard,
+  },
   options: { styleIsolation: "shared" },
   data() {
     let self = this;
