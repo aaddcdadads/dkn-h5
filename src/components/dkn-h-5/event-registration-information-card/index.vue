a73a3c6af0829c31241688a044c7f127d295ec99
胡小根 <hxg@haomo.com>
2024-02-04 11:25:22 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

---
+++ src/components/dkn-h-5/event-registration-information-card/index.vue
@@ -0,0 +1,251 @@
+<template>
+  <view
+    ref="package"
+    v-for="(item, index) in data"
+    class="goods"
+    :key="index"
+    @click="onClick(index)"
+  >
+    <view class="image">
+      <image :src="item.image" class="image"></image>
+    </view>
+    <view class="texts">
+      <text class="name">{{ item.name }}</text>
+      <text class="description">{{ item.description }}</text>
+      <view class="activity">
+        <text class="price">¥{{ item.price }}</text>
+        <text class="price" style="font-size: 13px;">x {{ item.number }}</text>
+        <!-- <view class="numberBox">
+          <view class="reduce" @click.stop="valChange(-1, index)">
+            <svg
+              t="1706272970101"
+              v-if="!item.number"
+              class="icon"
+              viewBox="0 0 1024 1024"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              p-id="5985"
+              width="16"
+              height="16"
+            >
+              <path
+                d="M779.184225 518.139837 244.815775 518.139837c-12.397355 0-22.429849-10.042727-22.429849-22.429849 0-12.387122 10.032494-22.429849 22.429849-22.429849l534.36845 0c12.397355 0 22.429849 10.042727 22.429849 22.429849C801.614074 508.09711 791.58158 518.139837 779.184225 518.139837z"
+                p-id="5986"
+                fill="#999999"
+              ></path>
+            </svg>
+            <svg
+              t="1706270980680"
+              v-if="item.number"
+              class="icon"
+              viewBox="0 0 1024 1024"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              p-id="5495"
+              width="16"
+              height="16"
+            >
+              <path
+                d="M779.184225 518.139837 244.815775 518.139837c-12.397355 0-22.429849-10.042727-22.429849-22.429849 0-12.387122 10.032494-22.429849 22.429849-22.429849l534.36845 0c12.397355 0 22.429849 10.042727 22.429849 22.429849C801.614074 508.09711 791.58158 518.139837 779.184225 518.139837z"
+                p-id="5496"
+                fill="#2c2c2c"
+              ></path>
+            </svg>
+          </view>
+          <view class="number">{{ item.number }}</view>
+          <view class="add" @click.stop="valChange(1, index)">
+            <svg
+              t="1706270847479"
+              class="icon"
+              viewBox="0 0 1024 1024"
+              version="1.1"
+              xmlns="http://www.w3.org/2000/svg"
+              p-id="4223"
+              width="16"
+              height="16"
+            >
+              <path
+                d="M780 480H244c-17.67 0-32 14.33-32 32s14.33 32 32 32h536c17.67 0 32-14.33 32-32s-14.33-32-32-32z"
+                p-id="4224"
+                fill="#2c2c2c"
+              ></path>
+              <path
+                d="M512 212c-17.67 0-32 14.33-32 32v536c0 17.67 14.33 32 32 32s32-14.33 32-32V244c0-17.67-14.33-32-32-32z"
+                p-id="4225"
+                fill="#2c2c2c"
+              ></path>
+            </svg>
+          </view>
+        </view> -->
+      </view>
+    </view>
+  </view>
+</template>
+<script>
+export default {
+  props: {
+    /**
+     * 奖品数据
+     */
+    list: {
+      type: Array,
+      default: function () {
+        return [
+          {
+            checked: true,
+            image:
+              "https://img.js.design/assets/img/65bc6430a6a4a8693349c559.png",
+            name: "活动名称活动名称活动名称活动名称活动名称活动名称活动名称活动名称活动名称活动名称活动名称活动名称活动名称",
+            description:
+              "时代看风使舵丰动名称活动名称活动名称活动名称活动名称活动名称活动名动名称活动名称活动名称活动名称活动名称活动名称活动名动名称活动名称活动名称活动名称活动名称活动名称活动名富的少年疯狂是否达到减肥的步伐简单方便的",
+            price: "29.9",
+            number: 0,
+          },
+          {
+            checked: false,
+            image:
+              "https://hm-static-img.oss-cn-beijing.aliyuncs.com/%E9%BB%98%E8%AE%A4logo.png",
+            name: "活动名称",
+            description:
+              "动名称活动名称活动名称活动名称活动名称活动名称活动名动名称活动名称活动名称活动名称活动名称活动名称活动名动名称活动名称活动名称活动名称活动名称活动名称活动名",
+            price: "0.0",
+            number: 0,
+          },
+        ];
+      },
+    },
+  },
+  watch: {
+    list: {
+      handler: function (val) {
+        this.data = val;
+      },
+      deep: true,
+      immediate: true,
+    },
+  },
+  data() {
+    return {
+      data: [],
+      value: 0,
+    };
+  },
+  mounted() {
+    this.data = this.list;
+  },
+  methods: {
+    onClick(e) {
+      this.$emit("onClick", e);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.image {
+  width: 120px;
+  height: 100%;
+}
+
+.texts {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  flex: 1;
+  height: 100%;
+  margin-left: 8px;
+  margin-top: 6px;
+  padding: 5px;
+}
+
+.name {
+  font-style: normal;
+  text-align: left;
+  line-height: 24px;
+  margin-top: 3px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 1;
+  width: 100%;
+}
+
+.description {
+  margin-top: 3px;
+  height: 40px;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 2;
+  font-size: 14px;
+  color: rgba(153, 153, 153, 1);
+}
+
+.price {
+  font-size: 18px;
+  color: rgba(198, 83, 123, 1);
+}
+
+.goods {
+  display: flex;
+  align-items: center;
+  border-radius: 5px;
+  height: 125px;
+  width: 100%;
+  padding-right: 5px;
+  margin-bottom: 8px;
+  background: #fafafa;
+  position: relative;
+}
+
+.change {
+  margin-right: 10px;
+  margin-left: 10px;
+  transform: scale(0.7);
+}
+
+.activity {
+  height: 27px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 3px;
+}
+
+.numberBox {
+  font-family: "PingFangSC-Regular", "PingFang SC", sans-serif;
+  font-size: 13px;
+  display: flex;
+  justify-content: space-between;
+  border-radius: 5px;
+  margin-left: 10px;
+  transform: scale(0.95);
+}
+
+.reduce {
+  border: 1px solid #e5e5e5;
+  border-radius: 4px;
+  margin-right: 5px;
+}
+
+.number {
+  width: 40px;
+  text-align: center;
+  padding-top: 3px;
+  border: 1px solid #e5e5e5;
+  border-radius: 4px;
+}
+
+.add {
+  border: 1px solid #e5e5e5;
+  border-radius: 4px;
+  margin-left: 5px;
+}
+
+.icon {
+  padding: 4px 3px 0px 3px;
+}
+
+</style>
