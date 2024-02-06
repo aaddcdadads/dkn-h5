bcb4948d50d9b968bc3164ef49c52043a650bb1d
胡小根 <hxg@haomo.com>
2024-02-06 14:10:24 +0800

    feat: NUSEP网站-更新设计云页面清单

--- src/logics/SharingPageGrooup.js
+++ src/logics/SharingPageGrooup.js
@@ -7,6 +7,36 @@ let self = {};
 // logic对象保存当前逻辑组对象，可以通过logic.func或者self.func来调用本逻辑组内的所有逻辑单元
 let logic = {};
 
+/********************** getCode 开始 *********************/
+/**
+ * 获取二维码请求
+ */
+const getCodeRequest = (logic.getCodeRequest = async function () {
+  let res = await self.$getAction(`/api/dkn/activity/list`, {
+    id: self.$route.query.activityId,
+  });
+  self.getCodeRequestData = res;
+});
+
+/**
+ * 逻辑流 getCode 入口函数
+ */
+const getCode = (logic.getCode = async (pageVm, eventData) => {
+  console.log(`getCode: `, pageVm, eventData);
+  self = Object.assign(pageVm, logic);
+  self.getCodeData = eventData;
+
+  await getCodeRequest();
+  if (self.getCodeRequestData.success) {
+  } else {
+  }
+  let records = self.sharingPageRequestData.result.records;
+  self.code.name = records[0].qrCode;
+  self.sharingImage.backgroundImage = records[0].url;
+});
+
+/********************** end getCode 开始 *********************/
+
 /********************** sharingPageInitialization 开始 *********************/
 /**
  * 分享页请求
@@ -40,4 +70,9 @@ const sharingPageInitialization = (logic.sharingPageInitialization = async (
 
 /********************** end sharingPageInitialization 开始 *********************/
 
-export { sharingPageRequest, sharingPageInitialization };
+export {
+  getCodeRequest,
+  getCode,
+  sharingPageRequest,
+  sharingPageInitialization,
+};
