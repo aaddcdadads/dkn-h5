a7eb68ef15c59aa118fb0963547240059d672497
胡小根 <hxg@haomo.com>
2024-02-07 17:48:58 +0800

    feat: NUSEP网站-更新设计云页面清单

--- src/pages/haomo/1750405451125624834/page.vue
+++ src/pages/haomo/1750405451125624834/page.vue
@@ -1,39 +1,23 @@
 <template>
   <view class="page card">
     <view class="page-wrapper">
-      <view
-        class="ele-wrapper ele-wrapper-b06d64c4-f458-43af-b486-f9fa892d1fa2"
-      >
-        <hm-uview-bg-card
-          width="100%"
-          height="100%"
-          border-radius="0"
-          padding="0"
-          box-shadow-v-shadow="0"
-          box-shadow-blur="0"
-          class="ele-b06d64c4-f458-43af-b486-f9fa892d1fa2"
-        >
-          <view class="ele-wrapper ele-wrapper-indexList">
-            <hm-index-list
-              ref="indexList"
-              :list="indexList.list"
-              :url="indexList.url"
-              :get-data-map="indexList.getDataMap"
-              :scroll-top="indexList.scrollTop"
-              class="ele-indexList"
-            >
-            </hm-index-list>
-          </view>
-        </hm-uview-bg-card>
-      </view>
+        <view class="ele-wrapper ele-wrapper-b06d64c4-f458-43af-b486-f9fa892d1fa2">
+      <hm-uview-bg-card width="100%" height="100%" border-radius="0" padding="0" box-shadow-v-shadow="0" box-shadow-blur="0" class="ele-b06d64c4-f458-43af-b486-f9fa892d1fa2"> 
+            <view class="ele-wrapper ele-wrapper-indexList">
+      <hm-index-list ref="indexList" :list="indexList.list" :url="indexList.url" :get-data-map="indexList.getDataMap" :scroll-top="indexList.scrollTop" class="ele-indexList"> 
+    </hm-index-list>
+    </view> 
+    </hm-uview-bg-card>
+    </view>
     </view>
   </view>
 </template>
 
 <script>
-import { h } from "vue";
+import { h } from 'vue';
 import HmUviewBgCard from "/@/components/built-in/uniapp-uview-vue3/HmUviewBgCard.vue";
 import HmIndexList from "/@/components/built-in/uniapp-haomo/HmIndexList.vue";
+
 
 export default {
   name: "Test",
@@ -45,50 +29,50 @@ export default {
   data() {
     let self = this;
     return {
-      indexList: {
-        list: [],
-        url: "/api/dkn/store/listOrder",
-        getDataMap: {
-          title: "title",
-          src: "src",
-          text: "text",
-          name: "key",
-          subName: "subName",
-        },
-        scrollTop: 0,
-      },
-    };
-  },
-  watch: {},
-  mounted(e) {
-    this.onMounted(e);
-  },
-  methods: {
-    onMounted() {
-      let url = "/api/dkn/store/listOrder";
-      let self = this;
-      self.$getAction(url, {}).then((res) => {
-        console.log("res", res.result);
-      });
+  "indexList": {
+    "list": [],
+    "url": "/api/dkn/store/listOrder",
+    "getDataMap": {
+      "name": "key"
     },
+    "scrollTop": 0
+  }
+};
   },
+  watch: {
+  },
+    mounted(e) {
+      this.onMounted(e);
+    },
+  methods:{
+    onMounted() {
+  let url = "/api/dkn/store/listOrder";
+  let self = this;
+  self.$getAction(url, {}).then((res) => {
+    console.log("res", res.result);
+  });
+
+},
+
+  }
 };
 </script>
 
 <style lang="less" scoped>
 .page {
-  /deep/ .page-wrapper {
-    width: 100%;
-    height: 100%;
-  }
+  /deep/ .page-wrapper{
+  width:100%;
+height:100%;
+}
 }
 
 .ele-wrapper-b06d64c4-f458-43af-b486-f9fa892d1fa2 {
-  width: 100%;
-  height: 100%;
+  width:100%;
+height:100%;
 }
 
 .ele-wrapper-indexList {
-  width: 100%;
+  width:100%;
 }
+
 </style>
