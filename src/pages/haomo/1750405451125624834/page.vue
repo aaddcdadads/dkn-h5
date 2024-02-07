b8b062d7c97524ac7606dfd49d17e2a270c61dea
胡小根 <hxg@haomo.com>
2024-02-07 16:44:49 +0800

    feat: NUSEP网站-更新设计云页面清单

--- src/pages/haomo/1750405451125624834/page.vue
+++ src/pages/haomo/1750405451125624834/page.vue
@@ -1,49 +1,51 @@
 <template>
   <view class="page card">
     <view class="page-wrapper">
-      <view
-        class="ele-wrapper ele-wrapper-b06d64c4-f458-43af-b486-f9fa892d1fa2"
-      >
-        <hm-uview-bg-card
-          width="100%"
-          height="100%"
-          border-radius="0"
-          padding="0"
-          box-shadow-v-shadow="0"
-          box-shadow-blur="0"
-          class="ele-b06d64c4-f458-43af-b486-f9fa892d1fa2"
-        >
-        </hm-uview-bg-card>
-      </view>
+        <view class="ele-wrapper ele-wrapper-b06d64c4-f458-43af-b486-f9fa892d1fa2">
+      <hm-uview-bg-card width="100%" height="100%" border-radius="0" padding="0" box-shadow-v-shadow="0" box-shadow-blur="0" class="ele-b06d64c4-f458-43af-b486-f9fa892d1fa2"> 
+            <view class="ele-wrapper ele-wrapper-b7585dbe-1163-4c41-ade4-cd5a0ad7d68e">
+      <hm-index-list :scroll-top="0"> 
+    </hm-index-list>
+    </view> 
+    </hm-uview-bg-card>
+    </view>
     </view>
   </view>
 </template>
 
 <script>
-import { h } from "vue";
+import { h } from 'vue';
 import HmUviewBgCard from "/@/components/built-in/uniapp-uview-vue3/HmUviewBgCard.vue";
+import HmIndexList from "/@/components/built-in/uniapp-haomo/HmIndexList.vue";
+
 
 export default {
   name: "Test",
   components: {
     HmUviewBgCard,
+    HmIndexList,
   },
   options: { styleIsolation: "shared" },
   data() {
     let self = this;
     return {};
   },
-  watch: {},
-  methods: {},
+  watch: {
+  },
+  methods:{
+
+  }
 };
 </script>
 
 <style lang="less" scoped>
 .page {
+  
 }
 
 .ele-wrapper-b06d64c4-f458-43af-b486-f9fa892d1fa2 {
-  width: 100%;
-  height: 400px;
+  width:100%;
+height:400px;
 }
+
 </style>
