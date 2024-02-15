79fd920a3dee11390b309a37a37982652ab21117
胡小根 <hxg@haomo.com>
2024-02-15 12:22:39 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

--- src/logics/SharingPageGrooup.js
+++ src/logics/SharingPageGrooup.js
@@ -11,28 +11,36 @@ let logic = {};
 /**
  * 获取二维码请求
  */
-const getCodeRequest = (logic.getCodeRequest = async function () {
-  let res = await self.$getAction(`/api/dkn/activity/list`, {
-    id: self.$route.query.activityId,
-  });
+const getCodeRequest = logic.getCodeRequest = async function () {
+  let res = await self.$getAction(
+    `/api/dkn/activity/list`,
+    {
+    id: self.$route.query.activityId
+}
+  )
   self.getCodeRequestData = res;
-});
+}
+
 
 /**
  * 逻辑流 getCode 入口函数
  */
-const getCode = (logic.getCode = async (pageVm, eventData) => {
-  console.log(`getCode: `, pageVm, eventData);
+const getCode = logic.getCode = async (pageVm, eventData) => {
+  console.log(`getCode: `, pageVm, eventData)
   self = Object.assign(pageVm, logic);
   self.getCodeData = eventData;
 
+  
   await getCodeRequest();
-  if (self.getCodeRequestData.success) {
-  } else {
-  }
-  let records = self.getCodeRequestData.result.records;
-  self.code.name = records[0].qrCode;
-});
+  if(self.getCodeRequestData.success){
+  
+}else{
+  
+}
+  let records = self.getCodeRequestData.result.records
+self.code.name = records[0].qrCode
+
+}
 
 /********************** end getCode 开始 *********************/
 
@@ -41,17 +49,16 @@ const getCode = (logic.getCode = async (pageVm, eventData) => {
 /**
  * 逻辑流 sharingPageInitialization 入口函数
  */
-const sharingPageInitialization = (logic.sharingPageInitialization = async (
-  pageVm,
-  eventData
-) => {
-  console.log(`sharingPageInitialization: `, pageVm, eventData);
+const sharingPageInitialization = logic.sharingPageInitialization = async (pageVm, eventData) => {
+  console.log(`sharingPageInitialization: `, pageVm, eventData)
   self = Object.assign(pageVm, logic);
   self.sharingPageInitializationData = eventData;
 
-  await getCode(self);
-  await getBackgroundImage(self);
-});
+  
+  await getCode(self, );
+  await getBackgroundImage(self, );
+
+}
 
 /********************** end sharingPageInitialization 开始 *********************/
 
@@ -59,36 +66,39 @@ const sharingPageInitialization = (logic.sharingPageInitialization = async (
 /**
  * 获取背景图请求
  */
-const getBackgroundImageRequest = (logic.getBackgroundImageRequest = async function () {
-  let res = await self.$getAction(`/api/dkn/activityImg/list`, {
+const getBackgroundImageRequest = logic.getBackgroundImageRequest = async function () {
+  let res = await self.$getAction(
+    `/api/dkn/activityImg/list`,
+    {
     activityId: self.$route.query.activityId,
-    type: 2,
-  });
+    type: 2
+}
+  )
   self.getBackgroundImageRequestData = res;
-});
+}
+
 
 /**
  * 逻辑流 getBackgroundImage 入口函数
  */
-const getBackgroundImage = (logic.getBackgroundImage = async (
-  pageVm,
-  eventData
-) => {
-  console.log(`getBackgroundImage: `, pageVm, eventData);
+const getBackgroundImage = logic.getBackgroundImage = async (pageVm, eventData) => {
+  console.log(`getBackgroundImage: `, pageVm, eventData)
   self = Object.assign(pageVm, logic);
   self.getBackgroundImageData = eventData;
 
+  
   await getBackgroundImageRequest();
-  if (self.getBackgroundImageRequestData.success) {
-  } else {
-  }
-  let records = self.getBackgroundImageRequestData.result.records;
-  if (records[0]) {
-    self.sharingImage.backgroundImage =
-      "/api/sys/common/static/" + records[0].path;
-  }
-  self.getQrCode = async function (id) {
-    let url = `http://dkn-h5.dev.haomo-tech.com?activityId=${self.$route.query.activityId}&channel=${self.$route.query.channel}`;
+  if(self.getBackgroundImageRequestData.success){
+  
+}else{
+  
+}
+  let records = self.getBackgroundImageRequestData.result.records
+if(records[0]){
+    self.sharingImage.backgroundImage = '/api/sys/common/static/' + records[0].path
+}
+self.getQrCode = async function(id) {
+    let url = `http://dkn-h5.dev.haomo-tech.com?activityId=${self.$route.query.activityId}&channel=${self.$route.query.channel}`
     setTimeout(() => {
       const container = document.querySelector(".ele-wrapper-code");
       console.log("container", container);
@@ -99,13 +109,13 @@ const getBackgroundImage = (logic.getBackgroundImage = async (
         colorDark: "#000000",
         colorLight: "#ffffff",
       });
-      self.urlInput.value = url;
     });
-    self.activityList.list = await self.getCode(id);
-  };
-});
+  }
+
+}
 
 /********************** end getBackgroundImage 开始 *********************/
+
 
 export {
   getCodeRequest,
@@ -113,4 +123,4 @@ export {
   sharingPageInitialization,
   getBackgroundImageRequest,
   getBackgroundImage,
-};
+}
