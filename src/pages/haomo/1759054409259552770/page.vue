8c049591a201c3eff2918ecebe1a02c8684f785b
胡小根 <hxg@haomo.com>
2024-02-18 11:24:05 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

--- src/pages/haomo/1759054409259552770/page.vue
+++ src/pages/haomo/1759054409259552770/page.vue
@@ -1,19 +1,19 @@
 <template>
   <view class="page card">
     <view class="page-wrapper">
-        <view class="ele-wrapper ele-wrapper-680618cc-bd18-4977-823b-01b21033f705">
-      <share-page class="ele-680618cc-bd18-4977-823b-01b21033f705"> 
-       
-    </share-page>
-    </view>
+      <view
+        class="ele-wrapper ele-wrapper-680618cc-bd18-4977-823b-01b21033f705"
+      >
+        <share-page class="ele-680618cc-bd18-4977-823b-01b21033f705">
+        </share-page>
+      </view>
     </view>
   </view>
 </template>
 
 <script>
-import { h } from 'vue';
+import { h } from "vue";
 import SharePage from "/@/components/dkn-h-5/share-page/index.vue";
-
 
 export default {
   name: "TestSharePage",
@@ -25,22 +25,17 @@ export default {
     let self = this;
     return {};
   },
-  watch: {
-  },
-  methods:{
-
-  }
+  watch: {},
+  methods: {},
 };
 </script>
 
 <style lang="less" scoped>
 .page {
-  
 }
 
 .ele-wrapper-680618cc-bd18-4977-823b-01b21033f705 {
   width: 100%;
-height: 100%;
+  height: 100%;
 }
-
 </style>
