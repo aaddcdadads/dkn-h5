49a50865e5fb4448603d0d2daf9d1eeaa0d4bf13
胡小根 <hxg@haomo.com>
2024-02-19 17:24:04 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

---
+++ src/pages/haomo/test/TestHmUviewFab.vue
@@ -0,0 +1,67 @@
+<template>
+  <view>
+    <hm-uview-fab></hm-uview-fab>
+    <hm-uview-fab
+      ref="test"
+      :pattern="test.pattern"
+      :horizontal="test.horizontal"
+      :vertical="test.vertical"
+      :popMenu="test.popMenu"
+      :content="test.content"
+      :direction="test.direction"
+      @trigger="onTrigger"
+      @fabClick="onFabClick"
+      @addClick="onAddClick"
+    ></hm-uview-fab>
+  </view>
+</template>
+<script>
+import HmUviewFab from "/@/components/built-in/uniapp-uview-vue3/HmUviewFab.vue";
+export default {
+  name:"TestHmUviewFab",
+  components:{HmUviewFab},
+  data() {
+    return {
+      test: {
+        pattern: {},
+        horizontal: "right",
+        vertical: "top",
+        popMenu: true,
+        content: [
+          {
+            iconPath: "/static/logo.png",
+            selectedIconPath: "/static/image-active.png",
+            text: "溜溜球",
+            active: false,
+          },
+          {
+            iconPath: "/static/logo.png",
+            selectedIconPath: "/static/home-active.png",
+            text: "激活",
+            active: false,
+          },
+          {
+            iconPath: "/static/logo.png",
+            selectedIconPath: "/static/star-active.png",
+            text: "收藏",
+            active: false,
+          },
+        ],
+        direction: "vertical",
+      },
+    };
+  },
+  methods: {
+    onTrigger(e) {
+      console.log("点击菜单项");
+    },
+    onFabClick(e) {
+      console.log("点击展开菜单项");
+    },
+    onAddClick(e) {
+      console.log("点击非菜单项");
+    },
+  },
+};
+</script>
+<style lang="less" scoped></style>
---
+++ src/pages/haomo/test/TestHmUviewImage.vue
@@ -0,0 +1,24 @@
+<template>
+  <view>
+    <Hm-uview-image />
+    <Hm-uview-image
+      src="https://scpic.chinaz.net/files/pic/pic9/202101/apic30599.jpg"
+      shape="circle"
+      width="200px"
+      height="200px"
+      :borderRedius="20"
+      loadingIcon="photo"
+    />
+  </view>
+</template>
+
+<script>
+import HmUviewImage from "@/components/built-in/uniapp-uview-vue3/HmUviewImage.vue";
+
+export default {
+  components: { HmUviewImage },
+};
+</script>
+
+<style>
+</style>
