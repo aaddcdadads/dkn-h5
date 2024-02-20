baad5956a07e6cbea1d8f2ac1ca616c0a160ea21
daijianhui <daijianhui@haomo-studio.com>
2024-02-20 17:16:53 +0800

    refactor: 增加dkn test环境的部署脚本

---
+++ deploy-dkn-test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -x
+rm -rf src/pages/haomo/test
+rm -rf src/pages/test
+# rm -rf src/components/built-in/third-party/HmMpHtml.vue
+# rm -rf src/components/built-in/third-party/HmNiceCropper.vue
+# rm -rf src/components/built-in/third-party/HmYqAvatar.vue
+# rm -rf src/components/built-in/third-party/HmLimeClipper.vue
+# rm -rf src/components/built-in/third-party/HmMonacoEditor.vue
+# rm -rf src/components/built-in/third-party/HmLottery.vue
+# rm -rf src/components/built-in/third-party/HmLimeSignature.vue
+./fix-component.sh
+npm run build:h5
+rsync -avz --delete --progress dist/build/h5/* sports@121.4.21.205:/var/www/html/dkn-h5-test
