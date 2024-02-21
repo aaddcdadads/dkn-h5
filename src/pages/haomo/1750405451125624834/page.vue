5d22a0de2f97367bc2971ccc9fa5ba50c42b98dd
胡小根 <hxg@haomo.com>
2024-02-21 15:31:21 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

--- src/pages/haomo/1750405451125624834/page.vue
+++ src/pages/haomo/1750405451125624834/page.vue
@@ -107,6 +107,125 @@
         >
         </hm-uview-radio>
       </view>
+      <view
+        class="ele-wrapper ele-wrapper-5bb7349d-5dc9-4f75-bf0d-810fed5e3beb"
+      >
+        <hm-uview-bg-card
+          width="100%"
+          height="300"
+          border-radius="0"
+          padding="0"
+          box-shadow-color="#00000000"
+          class="ele-5bb7349d-5dc9-4f75-bf0d-810fed5e3beb"
+        >
+          <view
+            class="ele-wrapper ele-wrapper-1106476f-b760-4878-b6cc-9bf76dd94a6b"
+          >
+            <hm-uview-radio
+              value=""
+              title=""
+              :list="[
+                { name: 'apple', disabled: false },
+                { name: 'banner', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+                { name: 'orange', disabled: false },
+              ]"
+              class="ele-1106476f-b760-4878-b6cc-9bf76dd94a6b"
+            >
+            </hm-uview-radio>
+          </view>
+        </hm-uview-bg-card>
+      </view>
     </view>
   </view>
 </template>
@@ -114,17 +233,400 @@
 <script>
 import { h } from "vue";
 import HmUviewRadio from "/@/components/built-in/uniapp-uview-vue3/HmUviewRadio.vue";
+import HmUviewBgCard from "/@/components/built-in/uniapp-uview-vue3/HmUviewBgCard.vue";
 
 export default {
   name: "Test",
   components: {
     HmUviewRadio,
+    HmUviewBgCard,
   },
   options: { styleIsolation: "shared" },
   data() {
     let self = this;
     return {
       "8901162e-2021-4f6d-8152-2c801ce4aa62": {
+        value: "",
+        list: [
+          {
+            name: "apple",
+            disabled: false,
+          },
+          {
+            name: "banner",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+          {
+            name: "orange",
+            disabled: false,
+          },
+        ],
+      },
+      "1106476f-b760-4878-b6cc-9bf76dd94a6b": {
         value: "",
         list: [
           {
@@ -537,5 +1039,32 @@ export default {
   }
   height: 300px;
   overflow: auto;
+}
+
+.ele-wrapper-5bb7349d-5dc9-4f75-bf0d-810fed5e3beb {
+  width: 100%;
+  height: 300px;
+  overflow: auto;
+}
+
+.ele-wrapper-1106476f-b760-4878-b6cc-9bf76dd94a6b {
+  width: 100%;
+  // /deep/.u-radio__icon-wrap--circle{
+  //     right: 0;
+  //     position: absolute;
+  // }
+  /deep/.u-radio {
+    width: 100% !important;
+    position: relative;
+    padding-left: 24rpx;
+    height: 72rpx;
+    border-bottom: 1px solid #f5f5f5 !important;
+  }
+  /deep/.u-clearfix {
+    width: 100% !important;
+  }
+  /deep/.u-radio__label {
+    font-size: 14px !important;
+  }
 }
 </style>
