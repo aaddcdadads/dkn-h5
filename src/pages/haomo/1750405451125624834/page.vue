93d36e879c5aa7cd392f2e372edafe51d21a3e28
胡小根 <hxg@haomo.com>
2024-02-21 11:02:51 +0800

    feat: NUSEP网站-更新设计云页面清单

--- src/pages/haomo/1750405451125624834/page.vue
+++ src/pages/haomo/1750405451125624834/page.vue
@@ -2,64 +2,24 @@
   <view class="page card">
     <view class="page-wrapper">
       <view
-        class="ele-wrapper ele-wrapper-b06d64c4-f458-43af-b486-f9fa892d1fa2"
+        class="ele-wrapper ele-wrapper-4ec22b24-aba6-4fa9-8493-da023b84bb8b"
       >
-        <hm-uview-bg-card
-          width="100%"
-          height="100%"
-          border-radius="0"
-          padding="0"
-          box-shadow-v-shadow="0"
-          box-shadow-blur="0"
-          class="ele-b06d64c4-f458-43af-b486-f9fa892d1fa2"
-        >
-          <view
-            class="ele-wrapper ele-wrapper-4b01ae23-2d04-43f2-a436-a9b3ce6bbfae"
-          >
-            <hm-uview-popup
-              :show="true"
-              :mode="'bottom'"
-              height="80%"
-              :mask-close-able="false"
-              class="ele-4b01ae23-2d04-43f2-a436-a9b3ce6bbfae"
-            >
-              <view
-                class="ele-wrapper ele-wrapper-4e111f2f-459b-40a0-9d39-a62c56425140"
-              >
-                <hm-uview-text
-                  text="选择门店"
-                  font-size="16px"
-                  class="ele-4e111f2f-459b-40a0-9d39-a62c56425140"
-                >
-                </hm-uview-text>
-              </view>
-              <view class="ele-wrapper ele-wrapper-indexListBox">
-                <hm-uview-bg-card
-                  ref="indexListBox"
-                  width="100%"
-                  height="100%"
-                  border-radius="0"
-                  padding="0"
-                  box-shadow-v-shadow="0"
-                  box-shadow-blur="0"
-                  class="ele-indexListBox"
-                >
-                  <view class="ele-wrapper ele-wrapper-indexList">
-                    <hm-index-list
-                      ref="indexList"
-                      :list="indexList.list"
-                      :url="indexList.url"
-                      :get-data-map="indexList.getDataMap"
-                      :scroll-top="indexList.scrollTop"
-                      class="ele-indexList"
-                    >
-                    </hm-index-list>
-                  </view>
-                </hm-uview-bg-card>
-              </view>
-            </hm-uview-popup>
-          </view>
-        </hm-uview-bg-card>
+        <u-radio> </u-radio>
+      </view>
+      <view
+        class="ele-wrapper ele-wrapper-d370b280-5a31-4f73-ad3f-84f77964c9d3"
+      >
+        <hm-uview-radio value=""> </hm-uview-radio>
+      </view>
+      <view
+        class="ele-wrapper ele-wrapper-cd19b0bd-38a8-46fe-904f-e40310103545"
+      >
+        <u-radio-group value="" size="" icon-size=""> </u-radio-group>
+      </view>
+      <view
+        class="ele-wrapper ele-wrapper-c557f40f-b1c1-4ee3-8328-af9f6ce8c51a"
+      >
+        <u-radio> </u-radio>
       </view>
     </view>
   </view>
@@ -67,88 +27,31 @@
 
 <script>
 import { h } from "vue";
-import HmUviewBgCard from "/@/components/built-in/uniapp-uview-vue3/HmUviewBgCard.vue";
-import HmUviewPopup from "/@/components/built-in/uniapp-uview-vue3/HmUviewPopup.vue";
-import HmUviewText from "/@/components/built-in/uniapp-uview-vue3/HmUviewText.vue";
-import HmIndexList from "/@/components/built-in/uniapp-haomo/HmIndexList.vue";
+import HmUviewRadio from "/@/components/built-in/uniapp-uview-vue3/HmUviewRadio.vue";
 
 export default {
   name: "Test",
   components: {
-    HmUviewBgCard,
-    HmUviewPopup,
-    HmUviewText,
-    HmIndexList,
+    HmUviewRadio,
   },
   options: { styleIsolation: "shared" },
   data() {
     let self = this;
     return {
-      "4b01ae23-2d04-43f2-a436-a9b3ce6bbfae": {
-        show: true,
+      "d370b280-5a31-4f73-ad3f-84f77964c9d3": {
+        value: "",
       },
-      indexList: {
-        list: [],
-        url: "/api/dkn/store/listOrder",
-        getDataMap: {
-          name: "key",
-        },
-        scrollTop: 0,
+      "cd19b0bd-38a8-46fe-904f-e40310103545": {
+        value: "",
       },
     };
   },
   watch: {},
-  mounted(e) {
-    this.onMounted(e);
-  },
-  methods: {
-    onMounted() {
-      let url = "/api/dkn/store/listOrder";
-      let self = this;
-      self.$getAction(url, {}).then((res) => {
-        console.log("res", res.result);
-      });
-    },
-  },
+  methods: {},
 };
 </script>
 
 <style lang="less" scoped>
 .page {
-  /deep/ .page-wrapper {
-    width: 100%;
-    height: 100%;
-  }
-}
-
-.ele-wrapper-b06d64c4-f458-43af-b486-f9fa892d1fa2 {
-  width: 100%;
-  height: 100%;
-}
-
-.ele-wrapper-4b01ae23-2d04-43f2-a436-a9b3ce6bbfae {
-  /deep/.uni-scroll-view-content {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-}
-
-.ele-wrapper-4e111f2f-459b-40a0-9d39-a62c56425140 {
-  font-size: 16px;
-  color: #333333;
-  line-height: 20px;
-  margin: 10px 0;
-
-  display: none;
-}
-
-.ele-wrapper-indexListBox {
-  width: 100%;
-  height: 100%;
-}
-
-.ele-wrapper-indexList {
-  width: 100%;
 }
 </style>
