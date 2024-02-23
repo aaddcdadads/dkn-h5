90c9fbf645cbd3643d6d89e143d064ff1ea8dbf2
胡小根 <hxg@haomo.com>
2024-02-23 11:47:06 +0800

    feat: NUSEP网站-更新设计云页面清单

---
+++ src/pages/haomo/test/TestHmLoop.vue
@@ -0,0 +1,59 @@
+<template>
+  <hm-loop :value="data">
+    <template #default="{ item }">
+      <button
+        :size="item.size"
+        :plain="item.plain"
+        :type="item.type"
+        :disabled="item.disabled"
+        shape="square"
+        @click="onClick"
+      >
+        {{ item.text }}
+      </button>
+    </template>
+  </hm-loop>
+</template>
+
+<script>
+import HmButton from "@/components/built-in/uniapp/HmButton.vue";
+import HmLoop from "@/components/built-in/uniapp-haomo/HmLoop.vue";
+
+export default {
+  components: { 
+    HmButton,
+    HmLoop 
+  },
+  data() {
+    return {
+      data: [{
+        size: 'mini',
+        plain: false,
+        type: 'primary',
+        disabled: false,
+        shape: 'square',
+        text: '按钮'
+      }, {
+        size: 'default',
+        plain: false,
+        type: 'warn',
+        disabled: false,
+        shape: 'square',
+        text: '按钮'
+      }, {
+        size: 'mini',
+        plain: true,
+        type: 'default',
+        disabled: true,
+        shape: 'square',
+        text: '按钮'
+      }],
+    };
+  },
+  methods: {
+  },
+};
+</script>
+
+<style lang="less">
+</style>
---
+++ src/pages/haomo/test/TestHmMumuPeview.vue
@@ -0,0 +1,19 @@
+<template>
+    <HmMumuPeview :fileUrl='fileUrl' v-model:value='showPreview'></HmMumuPeview>
+    <button @click='showPreview = true'>打开网络文件</button>
+  </template>
+  <script>
+  import HmMumuPeview from "@/components/built-in/uniapp-haomo/HmMumuPeview.vue";
+  export default {
+    components: {
+      HmMumuPeview,
+    },
+    data() {
+        return {
+            showPreview: false,
+            fileUrl: 'https://block-design.oss-cn-shenzhen.aliyuncs.com/preview-files/block-design-intro.pdf',
+            // fileUrl: 'https://block-design.oss-cn-shenzhen.aliyuncs.com/preview-files/block-design-intro.docx',
+        }
+    },
+  };
+  </script>
