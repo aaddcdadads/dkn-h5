44c24a8b47e83fdb9ef5dbfd5d3cd9c81ccca783
huangzhuoran <huangzhuoran@haomo-studio.com>
2024-03-04 13:14:16 +0800

    fix: appId添加环境变量

--- src/logics/pay.js
+++ src/logics/pay.js
@@ -52,7 +52,7 @@ function wxPay(orderId) {
   if (uAgent.indexOf("MicroMessenger") > 0) {
     //微信浏览器
     //跳转授权
-    let appId = "wx5110ac680933610e"
+    let appId = import.meta.env.VITE_APPID
     let redirectUri = import.meta.env.VITE_DOMAIN + "/pages/pay/wxPay/page"
     // let redirectUri = "http://192.168.202.158:3000/pages/pay/wxPay/page"
     location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=snsapi_base&state=${orderId}#wechat_redirect`
