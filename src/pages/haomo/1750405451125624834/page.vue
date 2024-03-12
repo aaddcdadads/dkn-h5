5fa3d5b1004d03a9ad7e751797a1ed7efff5f27f
胡小根 <hxg@haomo.com>
2024-03-12 16:03:00 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

--- src/pages/haomo/1750405451125624834/page.vue
+++ src/pages/haomo/1750405451125624834/page.vue
@@ -1,15 +1,24 @@
 <template>
   <view class="page card">
-    <view class="page-wrapper"> </view>
+    <view class="page-wrapper">
+      <view
+        class="ele-wrapper ele-wrapper-b186075d-5075-4af4-b62d-99ae640fb900"
+      >
+        <package-number-component> </package-number-component>
+      </view>
+    </view>
   </view>
 </template>
 
 <script>
 import { h } from "vue";
+import PackageNumberComponent from "/@/components/dkn-h-5/package-number-component/index.vue";
 
 export default {
   name: "Test",
-  components: {},
+  components: {
+    PackageNumberComponent,
+  },
   options: { styleIsolation: "shared" },
   data() {
     let self = this;
