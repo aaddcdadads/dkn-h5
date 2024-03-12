a47178a7185ae9a589d22341936f669a9b206d4f
胡小根 <hxg@haomo.com>
2024-03-12 15:47:33 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

--- src/pages/haomo/1750405451125624834/page.vue
+++ src/pages/haomo/1750405451125624834/page.vue
@@ -2,11 +2,6 @@
   <view class="page card">
     <view class="page-wrapper">
       <view
-        class="ele-wrapper ele-wrapper-aa0fdda1-0ce4-447f-8bd9-807c52416ed6"
-      >
-        <information-input-box value=""> </information-input-box>
-      </view>
-      <view
         class="ele-wrapper ele-wrapper-d0d10e22-b044-4597-a920-0b42ccc9a357"
       >
         <package-number-component
@@ -20,23 +15,17 @@
 
 <script>
 import { h } from "vue";
-import InformationInputBox from "/@/components/dkn-h-5/information-input-box/index.vue";
 import PackageNumberComponent from "/@/components/dkn-h-5/package-number-component/index.vue";
 
 export default {
   name: "Test",
   components: {
-    InformationInputBox,
     PackageNumberComponent,
   },
   options: { styleIsolation: "shared" },
   data() {
     let self = this;
-    return {
-      "aa0fdda1-0ce4-447f-8bd9-807c52416ed6": {
-        value: "",
-      },
-    };
+    return {};
   },
   watch: {},
   methods: {},
