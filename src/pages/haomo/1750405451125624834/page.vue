2a6d982ddeafee1dae760ddf1c121fdc21d2cc80
胡小根 <hxg@haomo.com>
2024-03-12 15:13:29 +0800

    feat: NUSEP网站-更新设计云页面清单

--- src/pages/haomo/1750405451125624834/page.vue
+++ src/pages/haomo/1750405451125624834/page.vue
@@ -1,34 +1,39 @@
 <template>
   <view class="page card">
     <view class="page-wrapper">
+      <view
+        class="ele-wrapper ele-wrapper-aa0fdda1-0ce4-447f-8bd9-807c52416ed6"
+      >
+        <information-input-box value=""> </information-input-box>
+      </view>
     </view>
   </view>
 </template>
 
 <script>
-import { h } from 'vue';
-
+import { h } from "vue";
+import InformationInputBox from "/@/components/dkn-h-5/information-input-box/index.vue";
 
 export default {
   name: "Test",
   components: {
+    InformationInputBox,
   },
   options: { styleIsolation: "shared" },
   data() {
     let self = this;
-    return {};
+    return {
+      "aa0fdda1-0ce4-447f-8bd9-807c52416ed6": {
+        value: "",
+      },
+    };
   },
-  watch: {
-  },
-  methods:{
-
-  }
+  watch: {},
+  methods: {},
 };
 </script>
 
 <style lang="less" scoped>
 .page {
-  
 }
-
 </style>
