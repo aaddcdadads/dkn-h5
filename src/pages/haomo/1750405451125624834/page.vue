163ad51d2fbf976e9bb5d527f77b0ecad4f3ed03
胡小根 <hxg@haomo.com>
2024-03-13 16:31:42 +0800

    feat: 迪卡侬春节活动-h5端-更新设计云页面清单

--- src/pages/haomo/1750405451125624834/page.vue
+++ src/pages/haomo/1750405451125624834/page.vue
@@ -5,9 +5,27 @@
         class="ele-wrapper ele-wrapper-b186075d-5075-4af4-b62d-99ae640fb900"
       >
         <package-number-component
+          :func-list="[
+            {
+              id: '1',
+              placeholder: '参与人真实姓名+身份证号',
+              disabled: false,
+            },
+            {
+              id: '2',
+              placeholder: '参与人真实姓名+身份证号',
+              disabled: false,
+            },
+          ]"
           class="ele-b186075d-5075-4af4-b62d-99ae640fb900"
         >
         </package-number-component>
+      </view>
+      <view
+        class="ele-wrapper ele-wrapper-39a6dc97-777e-4b33-95b1-3708bf923e63"
+      >
+        <registration-form-package-components>
+        </registration-form-package-components>
       </view>
     </view>
   </view>
@@ -16,11 +34,13 @@
 <script>
 import { h } from "vue";
 import PackageNumberComponent from "/@/components/dkn-h-5/package-number-component/index.vue";
+import RegistrationFormPackageComponents from "/@/components/dkn-h-5/registration-form-package-components/index.vue";
 
 export default {
   name: "Test",
   components: {
     PackageNumberComponent,
+    RegistrationFormPackageComponents,
   },
   options: { styleIsolation: "shared" },
   data() {
